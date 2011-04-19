fd604fcd93d2e933e48ea37463a0f4f8697c6a75
saikrishna1511@gmail.com <psaikrishna@ubudesk1004.(none)>
2011-04-19 18:21:36 +0530

    Management files except for add/delete user from group

---
+++ management/delgroup.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2010-2011 OpenStack, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import optparse
+import os
+import sqlite3
+
+
+def main():
+	usage = "usage: %prog group_id"
+	parser = optparse.OptionParser(usage)
+	options, args = parser.parse_args()
+	if len(args) != 1:
+		parser.error("Incorrect number of arguments")
+	else:
+		group_id = args[0]
+		dbpath = os.path.abspath(
+			os.path.join(os.path.dirname(__file__),'../db/keystone.db'))
+		con = sqlite3.connect(dbpath)
+		cur = con.cursor()
+		cur.execute(
+			"DELETE FROM groups WHERE group_id='%s'" % group_id)
+		con.commit()
+		con.close()
+
+if __name__ == '__main__':
+	main()
---
+++ management/getusers.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2010-2011 OpenStack, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import optparse
+import os
+import sqlite3
+
+def main():
+	usage = "usage: %prog tenant_id"
+	parser = optparse.OptionParser(usage)
+	options, args = parser.parse_args()
+	if len(args) != 1:
+		parser.error("Incorrect number of arguments")
+	else:
+		tenant_id = args[0]
+		
+		dbpath = os.path.abspath(
+			os.path.join(os.path.dirname(__file__),'../db/keystone.db'))
+		con = sqlite3.connect(dbpath)
+		cur = con.cursor()
+		cur.execute(
+					"select u.* from users u inner join user-tenants ut on 
+					u.username = ut.user_id where ut.tenant_id = '%s' " % (tenant_id)
+					)
+		con.commit()
+		print cur.fetchall()
+		con.close()
+		
+if __name__ == '__main__':
+	main()
