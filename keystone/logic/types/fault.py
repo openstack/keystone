323bac0551268afdeb780014126bcb0f28d141dc
Jorge L. Williams <jorge.williams@rackspace.com>
2011-04-21 05:41:19 -0500

    Output serialization of faults.

--- keystone/logic/types/fault.py
+++ keystone/logic/types/fault.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import simplejson as json
+from lxml import etree
 
 class IDMFault(Exception):
     "Base Exception type for all auth exceptions"
@@ -22,6 +24,7 @@ class IDMFault(Exception):
         self.__code = code
         self.__msg = msg
         self.__details = details
+        self.key = "idmFault"
 
     @property
     def message(self):
@@ -35,12 +38,35 @@ class IDMFault(Exception):
     def details(self):
         return self.__details
 
+    def to_xml(self):
+        dom = etree.Element(self.key,
+                            xmlns="http://docs.openstack.org/idm/api/v1.0")
+        dom.set("code", self.__code.__str__())
+        msg = etree.Element("message")
+        msg.text = self.__msg
+        dom.append(msg)
+        desc = etree.Element("details")
+        desc.text = self.__details
+        dom.append(desc)
+        return etree.tostring(dom)
+
+    def to_json(self):
+        fault = {}
+        fault["message"] = self.__msg
+        fault["details"] = self.__details
+        fault["code"] = self.__code.__str__()
+        ret = {}
+        ret[self.key] = fault
+        return json.dumps (ret)
+
 
 class ServiceUnavailableFault(IDMFault):
     "The auth service is unavailable"
 
     def __init__(self, msg, details=None, code=503):
         super(ServiceUnavailableFault, self).__init__(msg, details, code)
+        self.key = "serviceUnavailable"
+
 
 
 class BadRequestFault(IDMFault):
@@ -48,6 +74,7 @@ class BadRequestFault(IDMFault):
 
     def __init__(self, msg, details=None, code=400):
         super(BadRequestFault, self).__init__(msg, details, code)
+        self.key = "badRequest"
 
 
 class UnauthorizedFault(IDMFault):
@@ -55,6 +82,7 @@ class UnauthorizedFault(IDMFault):
 
     def __init__(self, msg, details=None, code=401):
         super(UnauthorizedFault, self).__init__(msg, details, code)
+        self.key = "unauthorized"
 
 
 class UserDisabledFault(IDMFault):
@@ -62,6 +90,7 @@ class UserDisabledFault(IDMFault):
 
     def __init__(self, msg, details=None, code=403):
         super(UserDisabledFault, self).__init__(msg, details, code)
+        self.key = "userDisabled"
 
 
 class ForbiddenFault(IDMFault):
@@ -69,6 +98,7 @@ class ForbiddenFault(IDMFault):
 
     def __init__(self, msg, details=None, code=403):
         super(ForbiddenFault, self).__init__(msg, details, code)
+        self.key = "forbidden"
 
 
 class ItemNotFoundFault(IDMFault):
@@ -76,6 +106,7 @@ class ItemNotFoundFault(IDMFault):
 
     def __init__(self, msg, details=None, code=404):
         super(ItemNotFoundFault, self).__init__(msg, details, code)
+        self.key = "itemNotFound"
 
 
 class TenantConflictFault(IDMFault):
@@ -83,6 +114,7 @@ class TenantConflictFault(IDMFault):
 
     def __init__(self, msg, details=None, code=409):
         super(TenantConflictFault, self).__init__(msg, details, code)
+        self.key = "tenantConflict"
 
 
 class OverlimitFault(IDMFault):
@@ -92,6 +124,7 @@ class OverlimitFault(IDMFault):
         super(OverlimitFault, self).__init__(msg, details, code)
         self.args = (code, msg, details, retry_at)
         self.__retry_at = retry_at
+        self.key = "overLimit"
 
     @property
     def retry_at(self):
