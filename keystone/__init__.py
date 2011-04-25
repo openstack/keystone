6731b9ffb8487d015bdae037289a53fe7419e5d5
Vishvananda Ishaya <vishvananda@gmail.com>
2011-04-24 17:11:30 -0700

    use relative import in init

--- keystone/__init__.py
+++ keystone/__init__.py
@@ -1,3 +1,3 @@
-from keystone.middleware.papiauth import filter_factory as papiauth_factory
-from keystone.auth_protocol.auth_protocol_token \
+from middleware.papiauth import filter_factory as papiauth_factory
+from auth_protocol.auth_protocol_token \
         import filter_factory as tokenauth_factory
