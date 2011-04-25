5bfc134079fc0844a4036b858196069c960a2a6e
Jesse Andrews <anotherjesse@gmail.com>
2011-04-24 19:27:35 -0700

    we don't need properties yet

--- keystone/logic/types/auth.py
+++ keystone/logic/types/auth.py
@@ -20,7 +20,7 @@ import simplejson as json
 import keystone.logic.types.fault as fault
 from lxml import etree
 
-
+# FIXME(ja): do we need a base class?
 class Credentials(object):
     "Base class for all auth credentials."
     __metaclass__ = ABCMeta
@@ -30,9 +30,9 @@ class PasswordCredentials(Credentials):
     "Credentials based on username, password, and (optional) tenant_id."
 
     def __init__(self, username, password, tenant_id):
-        self.__username = username
-        self.__password = password
-        self.__tenant_id = tenant_id
+        self.username = username
+        self.password = password
+        self.tenant_id = tenant_id
 
     @staticmethod
     def from_xml(xml_str):
@@ -77,114 +77,58 @@ class PasswordCredentials(Credentials):
             raise fault.BadRequestFault("Cannot parse password credentials",
                                         str(e))
 
-    @property
-    def username(self):
-        return self.__username
-
-    @property
-    def password(self):
-        return self.__password
-
-    @property
-    def tenant_id(self):
-        return self.__password
-
 
 class Token(object):
     "An auth token."
 
     def __init__(self, expires, token_id):
-        self.__expires = expires
-        self.__token_id = token_id
-
-    @property
-    def expires(self):
-        return self.__expires
-
-    @property
-    def token_id(self):
-        return self.__token_id
+        self.expires = expires
+        self.token_id = token_id
 
 
 class Group(object):
     "A group, optionally belonging to a tenant."
 
     def __init__(self, group_id, tenant_id):
-        self.__tenant_id = tenant_id
-        self.__group_id = group_id
-
-    @property
-    def group_id(self):
-        return self.__group_id
-
-    @property
-    def tenant_id(self):
-        return self.__tenant_id
+        self.tenant_id = tenant_id
+        self.group_id = group_id
 
 
 class Groups(object):
     "A collection of groups."
 
     def __init__(self, values, links):
-        self.__values = values
-        self.__links = links
-
-    @property
-    def values(self):
-        return self.__values
-
-    @property
-    def links(self):
-        return self.__links
+        self.values = values
+        self.links = links
 
 
 class User(object):
     "A user."
 
     def __init__(self, username, tenant_id, groups):
-        self.__username = username
-        self.__tenant_id = tenant_id
-        self.__groups = groups
-
-    @property
-    def username(self):
-        return self.__username
-
-    @property
-    def tenant_id(self):
-        return self.__tenant_id
-
-    @property
-    def groups(self):
-        return self.__groups
+        self.username = username
+        self.tenant_id = tenant_id
+        self.groups = groups
 
 
 class AuthData(object):
     "Authentation Infor returned upon successful login."
 
     def __init__(self, token, user):
-        self.__token = token
-        self.__user = user
-
-    @property
-    def user(self):
-        return self.__user
-
-    @property
-    def token(self):
-        return self.__token
+        self.token = token
+        self.user = user
 
     def to_xml(self):
         dom = etree.Element("auth",
                              xmlns="http://docs.openstack.org/idm/api/v1.0")
         token = etree.Element("token",
-                             expires=self.__token.expires.isoformat())
+                             expires=self.token.expires.isoformat())
         token.set("id", self.__token.token_id)
         user = etree.Element("user",
-                             username=self.__user.username,
-                             tenantId=self.__user.tenant_id)
+                             username=self.user.username,
+                             tenantId=self.user.tenant_id)
         groups = etree.Element("groups")
-        for group in self.__user.groups.values:
+        for group in self.user.groups.values:
             g = etree.Element("group",
                              tenantId=group.tenant_id)
             g.set("id", group.group_id)
@@ -196,13 +140,13 @@ class AuthData(object):
 
     def to_json(self):
         token = {}
-        token["id"] = self.__token.token_id
-        token["expires"] = self.__token.expires.isoformat()
+        token["id"] = self.token.token_id
+        token["expires"] = self.token.expires.isoformat()
         user = {}
-        user["username"] = self.__user.username
-        user["tenantId"] = self.__user.tenant_id
+        user["username"] = self.user.username
+        user["tenantId"] = self.user.tenant_id
         group = []
-        for g in self.__user.groups.values:
+        for g in self.user.groups.values:
             grp = {}
             grp["tenantId"] = g.tenant_id
             grp["id"] = g.group_id
