c631f5f531f18684b9537e55232c25e4ae8f1050
Ziad Sawalha <github@highbridgellc.com>
2011-04-25 18:12:13 -0500

    Fixing and documenting middleware

--- keystone/auth_protocol/auth_protocol_token.py
+++ keystone/auth_protocol/auth_protocol_token.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
 # Copyright (c) 2010-2011 OpenStack, LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,6 +17,32 @@
 # limitations under the License.
 # Not Yet PEP8 standardized
 
+
+"""
+TOKEN-BASED AUTH MIDDLEWARE
+
+This WSGI component performs multiple jobs:
+- it verifies that incoming client requests have valid tokens by verifying
+    tokens with the auth service.
+- it will reject unauthenticated requests UNLESS it is in 'delegated' mode,
+    which means the final decision is delegated to the service
+- it will collect and forward identity information from a valid token
+    such as user name, groups, etc...
+
+Refer to: http://wiki.openstack.org/openstack-authn
+
+
+HEADERS
+-------
+www-authenticate    : only used if this component is being used remotely
+HTTP_AUTHORIZATION  : basic auth password used to validate the connection
+HTTP_X_AUTHORIZATION: the client identity being passed in
+HTTP_X_AUTH_TOKEN   : the client token being passed in
+HTTP_X_STORAGE_TOKEN: the client token being passed in (legacy Rackspace use)
+                      to support cloud files
+
+"""
+
 import json
 from webob.exc import HTTPUnauthorized, Request
 
@@ -22,19 +50,31 @@ from keystone.common.bufferedhttp import http_connect_raw as http_connect
 
 
 class TokenAuth(object):
-    """Auth Middleware that uses the dev auth server."""
+    """Auth Middleware that handles token authentication with an auth service"""
 
     def __init__(self, app, conf):
-        print "Starting the new one"
-        self.app = app
+        print "Starting the Token Auth component"
+
         self.conf = conf
+        self.app = app #if app is not set, this should forward requests
+
+        # where to find the OpenStack service (if not in local WSGI chain)
+        self.service_host = conf.get('service_host', '127.0.0.1')
+        self.service_port = int(conf.get('service_port', 8090))
+
+        # where to find the auth service (we use this to validate tokens)
         self.auth_host = conf.get('auth_ip', '127.0.0.1')
         self.auth_port = int(conf.get('auth_port', 8080))
+
+        # used to verify this component with the OpenStack service
         self.auth_pass = conf.get('auth_pass', 'dTpw')
+
+        # delegated means we still allow unauthenticated requests through
         self.delegated = int(conf.get('delegated', 0))
+        
 
     def __call__(self, env, start_response):
-
+        print "Handling a token-auth client call"
         def custom_start_response(status, headers):
             if self.delegated:
                 headers.append(('WWW-Authenticate', "Basic realm='API Realm'"))
@@ -42,8 +82,10 @@ class TokenAuth(object):
 
         token = env.get('HTTP_X_AUTH_TOKEN', env.get('HTTP_X_STORAGE_TOKEN'))
         if token:
-            # NOTE(vish): Not sure what the logic behind this other token is
-            headers = {'X-Auth-Token': '999888777666'}
+            # this request is claiming it has a valid token, let's check
+            # with the auth service
+            headers = {"Content-type": "application/json",
+                        "Accept": "text/json"}
             conn = http_connect(self.auth_host, self.auth_port, 'GET',
                                 '/v1.0/token/%s' % token, headers=headers)
             resp = conn.getresponse()
@@ -53,12 +95,12 @@ class TokenAuth(object):
             #       (self.auth_host, self.auth_port, token)
             #resp = Request.blank(path).get_response(self.app)
             #data = resp.body
-            dict_response = json.loads(data)
-            user = dict_response['auth']['user']['username']
             if not str(resp.status).startswith('20'):
                 if self.delegated:
                     env['HTTP_X_IDENTITY_STATUS'] = "Invalid"
             else:
+                dict_response = json.loads(data)
+                user = dict_response['auth']['user']['username']
                 env['HTTP_X_AUTHORIZATION'] = "Proxy " + user
                 if self.delegated:
                     env['HTTP_X_IDENTITY_STATUS'] = "Confirmed"
--- keystone/middleware/papiauth.py
+++ keystone/middleware/papiauth.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
 # Copyright (c) 2010 OpenStack, LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,11 +16,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+"""
+Auth Middleware that handles auth for a service
+
+This module can be installed as a filter in front of your service to validate
+that requests are coming from a trusted component that has handled
+authenticating the call. If a call comes from an untrusted source, it will
+redirect it back to be properly authenticated. This is done by sending our a 305
+proxy redirect response with the URL for the auth service.
+
+The auth service settings are specified in the INI file (keystone.ini). The ini
+file is passed in as the WSGI config file when starting the service. For this
+proof of concept, the ini file is in echo/echo/echo.ini.
+
+In the current implementation use a basic auth password to verify that the
+request is coming from a valid auth component or service
+
+Refer to: http://wiki.openstack.org/openstack-authn
+
+
+HEADERS
+-------
+
+www-authenticate    : only used if this component is being used remotely
+HTTP_AUTHORIZATION  : basic auth password used to validate the connection
+HTTP_X_AUTHORIZATION: the client token being passed in
+HTTP_X_AUTH_TOKEN   : the client token being passed in
+HTTP_X_STORAGE_TOKEN: the client token being passed in (legacy Rackspace use)
+                      to support cloud files
+
+"""
+
 from webob.exc import HTTPUseProxy, HTTPUnauthorized
 
 
 class PAPIAuth(object):
-    """Auth Middleware that uses the dev auth server."""
+
+    # app is the downstream WSGI component, usually the OpenStack service
+    #
+    # if app is not provided, the assumption is this filter is being run
+    # from a separate server.
 
     def __init__(self, app, conf):
         self.app = app
@@ -27,9 +66,10 @@ class PAPIAuth(object):
         self.auth_pass = conf.get('auth_pass', 'dTpw')
         self.proxy_location = 'http://%s:%d/' % (self.auth_host,
             self.auth_port)
-        print 'starting PAPI Auth middleware'
+        print 'Starting PAPI Auth middleware'
 
     def __call__(self, env, start_response):
+        print "PAPIAuth handling a service call"
         # Make sure that the user has been authenticated by the Auth Service
         if 'HTTP_X_AUTHORIZATION' not in env:
             self.proxy_location = 'http://' + self.auth_host + ':' + \
