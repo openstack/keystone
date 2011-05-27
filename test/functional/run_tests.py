1c8cea8858832584a1f7bfc7faf4259e190b9972
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2011-05-27 14:59:34 -0500

    Build the skeleton necessary to run tests

---
+++ test/functional/run_tests.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# Copyright (c) 2011 OpenStack, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import dtest
+import sys
+
+import base
+
+
+def add_opts(opts):
+    """Adds options specific to this test suite."""
+
+    opts.add_option("-u", "--username",
+                    action="store", type="string", dest="username",
+                    help="The username to use to access Keystone.")
+    opts.add_option("-p", "--password",
+                    action="store", type="string", dest="password",
+                    help="The password to use to access Keystone.")
+    opts.add_option("-k", "--keystone",
+                    action="store", type="string", dest="keystone",
+                    help="The URL to use to access Keystone.")
+
+    return opts
+
+
+if __name__ == '__main__':
+    # Obtain the options
+    opts = add_opts(dtest.optparser(usage="%prog [options]"))
+
+    # Process command-line arguments, saving them so tests can get to
+    # them
+    (base.options, args) = opts.parse_args()
+
+    # Execute the test suite
+    sys.exit(dtest.main(**dtest.opts_to_args(base.options)))
