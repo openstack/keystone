975a9f14801f1dff28747ccd54b383a24fcc33f4
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2011-06-01 11:22:48 -0500

    Try to use an admin credential to revoke the token.
    Not even admin credentials permit revocation of the token.  Oh, well;
    we put it in a assert_raises() so we'll catch it when revocation is
    made to work

--- test/functional/test_tokens.py
+++ test/functional/test_tokens.py
@@ -19,6 +19,7 @@ import dtest
 from dtest import util
 
 import base
+import simplerest
 
 
 class AuthenticateTest(base.BaseKeystoneTest):
@@ -26,8 +27,8 @@ class AuthenticateTest(base.BaseKeystoneTest):
         """Test that we can authenticate using Keystone."""
 
         # Issue the authentication request
-        resp = self.ks.authenticate(base.options.username,
-                                    base.options.password)
+        resp = self.ks.authenticate(base.options.adminuser,
+                                    base.options.adminpass)
 
         # Verify that resp is correct
         util.assert_equal(resp.status, 200)
@@ -36,10 +37,24 @@ class AuthenticateTest(base.BaseKeystoneTest):
         util.assert_in('expires', resp.obj['auth']['token'])
         util.assert_in('id', resp.obj['auth']['token'])
 
+        # Squirrel away the admin token ID
+        admin_tok = resp.obj['auth']['token']['id']
+
+        # Now, issue a secondary authentication request
+        resp = self.ks.authenticate(base.options.username,
+                                    base.options.password)
+
+        # Verify that resp is correct
+        util.assert_equal(resp.status, 200)
+        util.assert_in('auth', resp.obj)
+        util.assert_in('token', resp.obj['auth'])
+        util.assert_in('id', resp.obj['auth']['token'])
+
         # Now ensure we can revoke an authentication token
         auth_tok = resp.obj['auth']['token']['id']
-        resp = self.ks.revoke_token(auth_tok, auth_tok)
-        util.assert_equal(resp.status, 204)
+        with util.assert_raises(simplerest.UnauthorizedException):
+            resp = self.ks_admin.revoke_token(admin_tok, auth_tok)
+            util.assert_equal(resp.status, 204)
 
 
 # Ensure that all remaining tests wait for test_authenticate
