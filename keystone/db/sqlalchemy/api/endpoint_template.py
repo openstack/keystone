2b348106339c0afd83bb143d3323e91edd5e1ec5
Dolph Mathews <dolph.mathews@rackspace.com>
2011-06-21 14:37:20 -0500

    Adding endpoint related files

---
+++ keystone/db/sqlalchemy/api/endpoint_template.py
@@ -0,0 +1,185 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from keystone.db.sqlalchemy import get_session, models, aliased
+
+def create(values):
+    endpoint_template = models.EndpointTemplates()
+    endpoint_template.update(values)
+    endpoint_template.save()
+    return endpoint_template
+
+
+def get(id, session=None):
+    if not session:
+        session = get_session()
+    result = session.query(models.EndpointTemplates).filter_by(id=id).first()
+    return result
+
+
+def get_all(session=None):
+    if not session:
+        session = get_session()
+    return session.query(models.EndpointTemplates).all()
+
+
+def get_page(marker, limit, session=None):
+    if not session:
+        session = get_session()
+
+    if marker:
+        return session.query(models.EndpointTemplates).filter("id>:marker").params(\
+                marker='%s' % marker).order_by(\
+                models.EndpointTemplates.id.desc()).limit(limit).all()
+    else:
+        return session.query(models.EndpointTemplates).order_by(\
+                            models.EndpointTemplates.id.desc()).limit(limit).all()
+
+
+def get_page_markers(marker, limit, session=None):
+    if not session:
+        session = get_session()
+    first = session.query(models.EndpointTemplates).order_by(\
+                        models.EndpointTemplates.id).first()
+    last = session.query(models.EndpointTemplates).order_by(\
+                        models.EndpointTemplates.id.desc()).first()
+    if first is None:
+        return (None, None)
+    if marker is None:
+        marker = first.id
+    next = session.query(models.EndpointTemplates).filter("id > :marker").params(\
+                    marker='%s' % marker).order_by(\
+                    models.EndpointTemplates.id).limit(limit).all()
+    prev = session.query(models.EndpointTemplates).filter("id < :marker").params(\
+                    marker='%s' % marker).order_by(\
+                    models.EndpointTemplates.id.desc()).limit(int(limit)).all()
+    if len(next) == 0:
+        next = last
+    else:
+        for t in next:
+            next = t
+    if len(prev) == 0:
+        prev = first
+    else:
+        for t in prev:
+            prev = t
+    if prev.id == marker:
+        prev = None
+    else:
+        prev = prev.id
+    if next.id == last.id:
+        next = None
+    else:
+        next = next.id
+    return (prev, next)
+
+
+def endpoint_get_by_tenant_get_page(tenant_id, marker, limit,
+                                        session=None):
+    if not session:
+        session = get_session()
+    if marker:
+        return session.query(models.Endpoints).\
+            filter(models.Endpoints.tenant_id == tenant_id).\
+            filter("id >= :marker").params(
+            marker='%s' % marker).order_by(
+            models.Endpoints.id).limit(limit).all()
+    else:
+        return session.query(models.Endpoints).\
+            filter(models.Endpoints.tenant_id == tenant_id).\
+            order_by(models.Endpoints.id).limit(limit).all()
+
+
+def endpoint_get_by_tenant_get_page_markers(tenant_id, marker, limit,
+                                                session=None):
+    if not session:
+        session = get_session()
+    tba = aliased(models.Endpoints)
+    first = session.query(tba).\
+                    filter(tba.tenant_id == tenant_id).\
+                    order_by(tba.id).first()
+    last = session.query(tba).\
+                filter(tba.tenant_id == tenant_id).\
+                order_by(tba.id.desc()).first()
+    if first is None:
+        return (None, None)
+    if marker is None:
+        marker = first.id
+    next = session.query(tba).\
+                filter(tba.tenant_id == tenant_id).\
+                filter("id>=:marker").params(
+                marker='%s' % marker).order_by(
+                tba.id).limit(int(limit)).all()
+
+    prev = session.query(tba).\
+                    filter(tba.tenant_id == tenant_id).\
+                    filter("id < :marker").params(
+                    marker='%s' % marker).order_by(
+                    tba.id).limit(int(limit) + 1).all()
+    next_len = len(next)
+    prev_len = len(prev)
+
+    if next_len == 0:
+        next = last
+    else:
+        for t in next:
+            next = t
+    if prev_len == 0:
+        prev = first
+    else:
+        for t in prev:
+            prev = t
+    if first.id == marker:
+        prev = None
+    else:
+        prev = prev.id
+    if marker == last.id:
+        next = None
+    else:
+        next = next.id
+    return (prev, next)
+
+
+def endpoint_add(values):
+    endpoints = models.Endpoints()
+    endpoints.update(values)
+    endpoints.save()
+    return endpoints
+
+
+def endpoint_get(id, session=None):
+    if not session:
+        session = get_session()
+    result = session.query(models.Endpoints).\
+                    filter_by(id=id).first()
+    return result
+
+
+def endpoint_get_by_tenant(tenant_id, session=None):
+    if not session:
+        session = get_session()
+    result = session.query(models.Endpoints).\
+                    filter_by(tenant_id=tenant_id).first()
+    return result
+
+
+def endpoint_delete(id, session=None):
+    if not session:
+        session = get_session()
+    with session.begin():
+        endpoints = endpoint_get(id, session)
+        session.delete(endpoints)
---
+++ keystone/logic/types/endpoint.py
@@ -0,0 +1,223 @@
+# Copyright (c) 2010-2011 OpenStack, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+from lxml import etree
+import string
+
+import keystone.logic.types.fault as fault
+
+
+class EndpointTemplate(object):
+    @staticmethod
+    def from_xml(xml_str):
+        try:
+            dom = etree.Element("root")
+            dom.append(etree.fromstring(xml_str))
+            root = dom.find("{http://docs.openstack.org/identity/api/v2.0}" \
+                            "endpointTemplate")
+            if root == None:
+                raise fault.BadRequestFault("Expecting endpointTemplate")
+            id = root.get("id")
+            region = root.get("region")
+            service = root.get("serviceName")
+            public_url = root.get("publicURL")
+            admin_url = root.get("adminURL")
+            internal_url = root.get("internalURL")
+            enabled = root.get("enabled")
+            return EndpointTemplate(id, region, service, public_url, admin_url,
+                           internal_url, enabled)
+        except etree.LxmlError as e:
+            raise fault.BadRequestFault("Cannot parse endpointTemplate", str(e))
+
+    @staticmethod
+    def from_json(json_str):
+        try:
+            obj = json.loads(json_str)
+            region = None
+            service = None
+            public_url = None
+            admin_url = None
+            internal_url = None
+            enabled = None
+
+            if not "endpointTemplate" in obj:
+                raise fault.BadRequestFault("Expecting endpointTemplate")
+            endpoint_template = obj["endpointTemplate"]
+
+            # Check that fields are valid
+            invalid = [key for key in endpoint_template if key not in
+                       ['id', 'region', 'serviceName', 'publicURL',
+                        'adminURL', 'internalURL', 'enabled']]
+            if invalid != []:
+                raise fault.BadRequestFault("Invalid attribute(s): %s"
+                                            % invalid)
+
+            if not "id" in endpoint_template:
+                id = None
+            else:
+                id = endpoint_template["id"]
+            if id == None:
+                raise fault.BadRequestFault("Expecting endpointTemplate")
+
+            if 'region' in endpoint_template:
+                region = endpoint_template["region"]
+            if 'serviceName' in endpoint_template:
+                service = endpoint_template["serviceName"]
+            if 'publicURL' in endpoint_template:
+                public_url = endpoint_template["publicURL"]
+            if 'adminURL' in endpoint_template:
+                admin_url = endpoint_template["adminURL"]
+            if 'internalURL' in endpoint_template:
+                internal_url = endpoint_template["internalURL"]
+            if 'enabled' in endpoint_template:
+                enabled = endpoint_template["enabled"]
+
+            return EndpointTemplate(id, region, service, public_url, admin_url,
+                           internal_url, enabled)
+        except (ValueError, TypeError) as e:
+            raise fault.BadRequestFault("Cannot parse endpointTemplate", str(e))
+
+    def __init__(self, id, region, service, public_url, admin_url,
+                 internal_url, enabled):
+        self.id = id
+        self.region = region
+        self.service = service
+        self.public_url = public_url
+        self.admin_url = admin_url
+        self.internal_url = internal_url
+        self.enabled = enabled
+
+    def to_dom(self):
+        dom = etree.Element("endpointTemplate",
+                        xmlns="http://docs.openstack.org/identity/api/v2.0")
+        if self.id:
+            dom.set("id", str(self.id))
+        if self.region:
+            dom.set("region", self.region)
+        if self.service:
+            dom.set("serviceName", self.service)
+        if self.public_url:
+            dom.set("publicURL", self.public_url)
+        if self.admin_url:
+            dom.set("adminURL", self.admin_url)
+        if self.internal_url:
+            dom.set("internalURL", self.internal_url)
+        if self.enabled:
+            dom.set("enabled", 'true')
+        return dom
+
+    def to_xml(self):
+        return etree.tostring(self.to_dom())
+
+    def to_dict(self):
+        endpoint_template = {}
+        if self.id:
+            endpoint_template["id"] = self.id
+        if self.region:
+            endpoint_template["region"] = self.region
+        if self.service:
+            endpoint_template["serviceName"] = self.service
+        if self.public_url:
+            endpoint_template["publicURL"] = self.public_url
+        if self.admin_url:
+            endpoint_template["adminURL"] = self.admin_url
+        if self.internal_url:
+            endpoint_template["internalURL"] = self.internal_url
+        if self.enabled:
+            endpoint_template["enabled"] = self.enabled
+        return {'endpointTemplate': endpoint_template}
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+
+class EndpointTemplates(object):
+    "A collection of endpointTemplates."
+
+    def __init__(self, values, links):
+        self.values = values
+        self.links = links
+
+    def to_xml(self):
+        dom = etree.Element("endpointTemplates")
+        dom.set(u"xmlns", "http://docs.openstack.org/identity/api/v2.0")
+
+        for t in self.values:
+            dom.append(t.to_dom())
+
+        for t in self.links:
+            dom.append(t.to_dom())
+
+        return etree.tostring(dom)
+
+    def to_json(self):
+        values = [t.to_dict()["endpointTemplate"] for t in self.values]
+        links = [t.to_dict()["links"] for t in self.links]
+        return json.dumps({"endpointTemplates": {"values": values, "links": links}})
+
+
+class Endpoint(object):
+    def __init__(self, id, href):
+        self.id = id
+        self.href = href
+
+    def to_dom(self):
+        dom = etree.Element("endpoint",
+                        xmlns="http://docs.openstack.org/identity/api/v2.0")
+        if self.id:
+            dom.set("id", str(self.id))
+        if self.href:
+            dom.set("href", self.href)
+        return dom
+
+    def to_xml(self):
+        return etree.tostring(self.to_dom())
+
+    def to_dict(self):
+        endpoint = {}
+        if self.id:
+            endpoint["id"] = self.id
+        if self.href:
+            endpoint["href"] = self.href
+        return {'endpoint': endpoint}
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+
+class Endpoints(object):
+    "A collection of endpoints."
+
+    def __init__(self, values, links):
+        self.values = values
+        self.links = links
+
+    def to_xml(self):
+        dom = etree.Element("endpoints")
+        dom.set(u"xmlns", "http://docs.openstack.org/identity/api/v2.0")
+
+        for t in self.values:
+            dom.append(t.to_dom())
+
+        for t in self.links:
+            dom.append(t.to_dom())
+
+        return etree.tostring(dom)
+
+    def to_json(self):
+        values = [t.to_dict()["endpoint"] for t in self.values]
+        links = [t.to_dict()["links"] for t in self.links]
+        return json.dumps({"endpoints": {"values": values, "links": links}})
