8e85047568a50b3857488a6cc6ded09941d23be9
Dolph Mathews <dolph.mathews@rackspace.com>
2011-07-12 13:58:00 -0500

    Moved db imports to config module Removed useless try/except blocks

--- keystone/config.py
+++ keystone/config.py
@@ -1,5 +1,10 @@
-from keystone.logic.service import IdentityService
-
 VERSION_STATUS = "ALPHA"
 VERSION_DATE = "2011-04-23T00:00:00Z"
+
+from keystone.logic.service import IdentityService
 SERVICE = IdentityService()
+
+# These just need to be imported somewhere, nothing appears to access them
+from keystone.backends import alterdb, sqlalchemy
+ALTERDB = alterdb
+SQLALCHEMY = sqlalchemy
