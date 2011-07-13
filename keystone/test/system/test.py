02360171a41f7b0a644e5cfc557142ae365c6a2d
Dolph Mathews <dolph.mathews@rackspace.com>
2011-07-13 11:52:54 -0500

    Converted system test framework to use httplib

--- keystone/test/system/test.py
+++ keystone/test/system/test.py
@@ -1,61 +1,70 @@
 import unittest
-import urllib2
+import httplib
 
 class RestfulTestCase(unittest.TestCase):
+    """Performs generic HTTP request testing"""
+    
     def setUp(self):
-        self.opener = urllib2.build_opener(urllib2.HTTPHandler)
+        """Sets default connection settings"""
+        self.host = '127.0.0.1'
+        self.port = 80
     
-    def tearDown(self):
-        self.opener = None
+    def request(self, method='GET', path='/', headers={}, body=None,
+            expect_exception=False):
+        """Perform request and fetch httplib.HTTPResponse from the server"""
+        
+        # Initialize a connection
+        connection = httplib.HTTPConnection(self.host, self.port, timeout=3)
+        
+        # Perform the request
+        connection.request(method, path, body, headers)
+        
+        # Retrieve the response so can go ahead and close the connection
+        response = connection.getresponse()
+        
+        # Close the connection
+        connection.close()
+        
+        # Automatically assert HTTP status code
+        if not expect_exception:
+            self.assertSuccessfulResponse(response.status)
+        else:
+            self.assertExceptionalResponse(response.status)
+        
+        # This contains the response headers, body, etc
+        return response
     
-    def request(self, method='GET', path='/', headers={}, data=None):
-        # Build URL
-        url = ''.join(['http://127.0.0.1', path])
-        
-        # Build request
-        request = urllib2.Request(url, data=data)
-        request.get_method = lambda: method
-        
-        # Build headers
-        for header, value in headers:
-            request.add_header(header, value)
-        
-        return self.opener.open(request)
+    def assertSuccessfulResponse(self, status_code):
+        """Asserts that a status code lies in the 2xx range"""
+        self.assertTrue(status_code >= 200 and status_code <= 299)
 
-class ServiceRestfulTestCase(RestfulTestCase):
-    def request(self, path='/', data=None, headers={}, method='GET'):
-        # Build URL
-        path = ''.join([':5000', path])
-        
-        # Build request
-        return super(AdminRestfulTestCase, self).request(
-            method=method,
-            path=path,
-            headers={},
-            data=data)
+    def assertExceptionalResponse(self, status_code):
+        """Asserts that a status code lies outside the 2xx range"""
+        self.assertTrue(status_code < 200 or status_code > 299)
 
-class AdminRestfulTestCase(RestfulTestCase):
-    def request(self, path='/', data=None, headers={}, method='GET'):
-        # Build URL
-        path = ''.join([':5001', path])
-        
-        # Build request
-        return super(AdminRestfulTestCase, self).request(
-            method=method,
-            path=path,
-            headers={},
-            data=data)
+class ServiceTestCase(RestfulTestCase):
+    """Perform generic HTTP request testing against Service API"""
     
-class TestContentTypes(AdminRestfulTestCase):
     def setUp(self):
-        super(TestContentTypes, self).setUp()
+        """Sets custom connection settings"""
+        super(ServiceTestCase, self).setUp()
+        
+        # Override parent's connection settings
+        self.port = 5000 # The port the service API is expected to run on
+        
+class AdminTestCase(RestfulTestCase):
+    """Perform generic HTTP request testing against Service API"""
     
-    def tearDown(self):
-        super(TestContentTypes, self).tearDown()
+    def setUp(self):
+        """Sets custom connection settings"""
+        super(AdminTestCase, self).setUp()
+        
+        # Override parent's connection settings
+        self.port = 5001 # The port the admin API is expected to run on
     
+class TestContentTypes(AdminTestCase):
     def test_simple(self):
-        r = self.request(path='/v2.0/')
-        self.assertEqual(r.status, 200)
+        self.request(path='/v2.0/')
 
 if __name__ == '__main__':
     unittest.main()
