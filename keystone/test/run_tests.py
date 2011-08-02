8b316b713098ee5d2f5d6722cc1af04b6f3d93b9
Dolph Mathews <dolph.mathews@rackspace.com>
2011-08-02 16:23:50 -0500

    Issue #115: Added support for testing multiple keystone configurations (sql-only, memcache, ldap)
    Change-Id: Ia539b93d244f98fdbf20692003f34edf2b9276aa

--- keystone/test/run_tests.py
+++ keystone/test/run_tests.py
@@ -1,41 +1,54 @@
+"""Manages execution of keystone test suites"""
 import os
 import sys
 import subprocess
 import time
 
+TEST_DIR = os.path.dirname(__file__)
+
+CONFIG_FILES = (
+    'keystone.sql.conf',
+    'keystone.memcache.conf',
+    'keystone.ldap.conf')
+
+TEMP_FILES = (
+    'keystone.db',
+    'keystone.token.db',
+    'ldap.db',
+    'ldap.db.db')
+
+def delete_temp_files():
+    """Quietly deletes any temp files in the test directory"""
+    for path in TEMP_FILES:
+        subprocess.call(['rm', '-f', os.path.join(TEST_DIR, path)])
+
 if __name__ == '__main__':
-    test_dir = os.path.dirname(__file__)
+    for config in CONFIG_FILES:
+        # remove any pre-existing temp files
+        delete_temp_files()
 
-    #remove pre-existing test databases
-    subprocess.call(['rm', os.path.join(test_dir, 'keystone.db')])
-    subprocess.call(['rm', os.path.join(test_dir, 'keystone.token.db')])
-    subprocess.call(['rm', os.path.join(test_dir, 'ldap.db')])
-    subprocess.call(['rm', os.path.join(test_dir, 'ldap.db.db')])
-
-    # populate the test database
-    subprocess.check_call([os.path.join(test_dir, '../../bin/sampledata.sh')])
-
-    try:
-        # run the keystone server
-        server = subprocess.Popen([os.path.join(test_dir,
-                                                '../../bin/keystone')])
-
-        # blatent hack.
-        time.sleep(3)
-        if server.poll() is not None:
-            print >> sys.stderr, 'Failed to start server'
-            sys.exit(-1)
+        # populate the test database
+        subprocess.check_call([
+            os.path.join(TEST_DIR, '..', '..', 'bin', 'sampledata.sh'),
+            '-c', os.path.join(TEST_DIR, '..', '..', config)])
 
         try:
-            # run tests
-            subprocess.check_call([
-                'unit2', 'discover', 'keystone.test'])
+            # run the keystone SERVER
+            SERVER = subprocess.Popen([
+                os.path.join(TEST_DIR, '..', '..', 'bin', 'keystone'),
+                '-c', os.path.join(TEST_DIR, '..', '..', config)])
+
+            # blatent hack.
+            time.sleep(3)
+            if SERVER.poll() is not None:
+                print >> sys.stderr, 'Failed to start SERVER'
+                sys.exit(-1)
+
+            try:
+                # run tests
+                subprocess.check_call(['unit2', 'discover', 'keystone.test'])
+            finally:
+                #kill the keystone SERVER
+                SERVER.kill()
         finally:
-            #kill the keystone server
-            server.kill()
-    finally:
-        # remove test databases
-        subprocess.call(['rm', os.path.join(test_dir, 'keystone.db')])
-        subprocess.call(['rm', os.path.join(test_dir, 'keystone.token.db')])
-        subprocess.call(['rm', os.path.join(test_dir, 'ldap.db')])
-        subprocess.call(['rm', os.path.join(test_dir, 'ldap.db.db')])
+            delete_temp_files()
