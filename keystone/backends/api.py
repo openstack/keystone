e3b9d24fea34b405a2c67cccd0c1955b35fb9c6b
Ziad Sawalha <github@highbridgellc.com>
2011-08-04 12:33:17 -0500

    Small licensing change to test Gerrit
    Change-Id: I2c7b3a9aa2eb8e7d1118c8e72bb9e1af00b9f8d5

--- keystone/backends/api.py
+++ keystone/backends/api.py
@@ -1,3 +1,7 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010 OpenStack LLC.
+# All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
