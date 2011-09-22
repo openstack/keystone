538c605bf2656bee01323d3753e7052296c04370
Anthony Young <sleepsonthefloor@gmail.com>
2011-09-22 15:12:07 -0700

    Fix for bug 856846 - cast ints to string in users_get_by_tenant_get_page so that they can be joined
    Change-Id: I4bc86a74acb8999e37a60746da4e646a9c66a2bf

--- keystone/backends/sqlalchemy/api/user.py
+++ keystone/backends/sqlalchemy/api/user.py
@@ -255,7 +255,7 @@ class UserAPI(BaseUserAPI):
                          order_by("id").\
                          limit(limit).\
                          all()
-        user_ids = set([assoc.user_id for assoc in rv])
+        user_ids = set([str(assoc.user_id) for assoc in rv])
         users = session.query(models.User).\
                       filter("id in ('%s')" % "','".join(user_ids)).\
                       all()
