a200e5007e0b0ba5b88a3555dea5600a1915701c
termie <github@anarkystic.com>
2011-10-11 15:11:27 -0700

    add a test client

--- keystonelight/test.py
+++ keystonelight/test.py
@@ -1,9 +1,36 @@
 import os
 import unittest
 
+from keystonelight import wsgi
+
 
 ROOTDIR = os.path.dirname(os.path.dirname(__file__))
 VENDOR = os.path.join(ROOTDIR, 'vendor')
+
+class TestClient(object):
+  def __init__(self, endpoint=None, token=None):
+    self.endpoint = None
+    self.token = token
+
+  def request(self, method, path, headers=None, body=None):
+    if headers is None:
+      headers = {}
+    req = wsgi.Request.blank(path)
+    req.method = method
+    for k, v in headers.iteritems():
+      req.headers[k] = v
+    if req.body:
+      req.body = body
+    return req.get_response(self.endpoint)
+
+  def get(self, path, headers=None):
+    return self.request('GET', path=path, headers=headers)
+
+  def post(self, path, headers=None, body=None):
+    return self.request('POST', path=path, headers=headers, body=body)
+
+  def put(self, path, headers=None, body=None):
+    return self.request('PUT', path=path, headers=headers, body=body)
 
 
 class TestCase(unittest.TestCase):
