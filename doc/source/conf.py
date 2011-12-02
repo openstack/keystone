849eb57ad533dfb7254af307776243cfdd7c804d
Pádraig Brady <P@draigBrady.com>
2011-12-02 12:24:30 +0000

    Bug#899116: use correct module when building docs
    Currently the wrong directory is searched for the
    keystone module, requiring keystone to be installed
    in the default system path, for docs to be buildable.
    Also the path was appended, so the default system version
    which may be completely different is referenced.
    This ensures that only local keystone files are referenced.
    
    Change-Id: I187a5153d9c8be076e8aff6bb0ae63a7c739e3e8

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -33,10 +33,10 @@ import sys
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append([os.path.abspath('../keystone'),
-    os.path.abspath('..'),
-    os.path.abspath('../bin')
-    ])
+sys.path = [os.path.abspath('../../keystone'),
+    os.path.abspath('../..'),
+    os.path.abspath('../../bin')
+    ] + sys.path
 
 # -- General configuration ---------------------------------------------------
 
