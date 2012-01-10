230a00386014c06e01b9d79a641640f4d6d6e764
termie <github@anarkystic.com>
2012-01-09 21:30:09 -0800

    add id-only flag to return IDs

--- keystone/config.py
+++ keystone/config.py
@@ -6,6 +6,9 @@ import os
 from keystone import cfg
 
 
+gettext.install('keystone', unicode=1)
+
+
 class Config(cfg.CommonConfigOpts):
     def __call__(self, config_files=None, *args, **kw):
         if config_files is not None:
@@ -21,20 +24,6 @@ class Config(cfg.CommonConfigOpts):
     def iteritems(self):
         for k in self._opts:
             yield (k, getattr(self, k))
-
-
-def register_str(*args, **kw):
-    group = kw.pop('group', None)
-    if group:
-        CONF.register_group(cfg.OptGroup(name=group))
-    return CONF.register_opt(cfg.StrOpt(*args, **kw), group=group)
-
-
-def register_cli_str(*args, **kw):
-    group = kw.pop('group', None)
-    if group:
-        CONF.register_group(cfg.OptGroup(name=group))
-    return CONF.register_cli_opt(cfg.StrOpt(*args, **kw), group=group)
 
 
 def setup_logging(conf):
@@ -84,14 +73,41 @@ def setup_logging(conf):
     root_logger.addHandler(handler)
 
 
-gettext.install('keystone', unicode=1)
+def register_str(*args, **kw):
+    group = _ensure_group(kw)
+    return CONF.register_opt(cfg.StrOpt(*args, **kw), group=group)
+
+
+def register_cli_str(*args, **kw):
+    group = _ensure_group(kw)
+    return CONF.register_opt(cfg.StrOpt(*args, **kw), group=group)
+
+
+def register_bool(*args, **kw):
+    group = _ensure_group(kw)
+    return CONF.register_opt(cfg.BoolOpt(*args, **kw), group=group)
+
+
+def register_cli_bool(*args, **kw):
+    group = _ensure_group(kw)
+    return CONF.register_cli_opt(cfg.BoolOpt(*args, **kw), group=group)
+
+
+def _ensure_group(kw):
+    group = kw.pop('group', None)
+    if group:
+        CONF.register_group(cfg.OptGroup(name=group))
+    return group
+
 
 CONF = Config(project='keystone')
+
 
 register_str('admin_token', default='ADMIN')
 register_str('compute_port')
 register_str('admin_port')
 register_str('public_port')
+
 
 # sql options
 register_str('connection', group='sql')
@@ -99,6 +115,7 @@ register_str('idle_timeout', group='sql')
 register_str('min_pool_size', group='sql')
 register_str('maz_pool_size', group='sql')
 register_str('pool_timeout', group='sql')
+
 
 register_str('driver', group='catalog')
 register_str('driver', group='identity')
