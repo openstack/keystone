ec8574928c929260b74c7a72aa8db9f66e103c67
termie <github@anarkystic.com>
2012-01-10 16:45:39 -0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='keystone',
       author_email='openstack@lists.launchpad.net',
       url='http://www.openstack.org',
       packages=find_packages(exclude=['test', 'bin']),
-      scripts=['bin/keystone', 'bin/ks'],
+      scripts=['bin/keystone', 'bin/keystone-manage'],
       zip_safe=False,
       install_requires=['setuptools'],
       )
