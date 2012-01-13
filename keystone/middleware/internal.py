8c33e66f1d1fe4a439b27a52584437174d14cd60
termie <github@anarkystic.com>
2012-01-12 16:07:23 -0800

    re-indent

--- keystone/middleware/internal.py
+++ keystone/middleware/internal.py
@@ -1,3 +1,5 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
 import json
 
 from keystone import config
@@ -20,79 +22,79 @@ PARAMS_ENV = 'openstack.params'
 
 
 class TokenAuthMiddleware(wsgi.Middleware):
-  def process_request(self, request):
-    token = request.headers.get(AUTH_TOKEN_HEADER)
-    context = request.environ.get(CONTEXT_ENV, {})
-    context['token_id'] = token
-    request.environ[CONTEXT_ENV] = context
+    def process_request(self, request):
+        token = request.headers.get(AUTH_TOKEN_HEADER)
+        context = request.environ.get(CONTEXT_ENV, {})
+        context['token_id'] = token
+        request.environ[CONTEXT_ENV] = context
 
 
 class AdminTokenAuthMiddleware(wsgi.Middleware):
-  """A trivial filter that checks for a pre-defined admin token.
+    """A trivial filter that checks for a pre-defined admin token.
 
-  Sets 'is_admin' to true in the context, expected to be checked by
-  methods that are admin-only.
+    Sets 'is_admin' to true in the context, expected to be checked by
+    methods that are admin-only.
 
-  """
+    """
 
-  def process_request(self, request):
-    token = request.headers.get(AUTH_TOKEN_HEADER)
-    context = request.environ.get(CONTEXT_ENV, {})
-    context['is_admin'] = (token == CONF.admin_token)
-    request.environ[CONTEXT_ENV] = context
+    def process_request(self, request):
+        token = request.headers.get(AUTH_TOKEN_HEADER)
+        context = request.environ.get(CONTEXT_ENV, {})
+        context['is_admin'] = (token == CONF.admin_token)
+        request.environ[CONTEXT_ENV] = context
 
 
 class PostParamsMiddleware(wsgi.Middleware):
-  """Middleware to allow method arguments to be passed as POST parameters.
+    """Middleware to allow method arguments to be passed as POST parameters.
 
-  Filters out the parameters `self`, `context` and anything beginning with
-  an underscore.
+    Filters out the parameters `self`, `context` and anything beginning with
+    an underscore.
 
-  """
+    """
 
-  def process_request(self, request):
-    params_parsed = request.params
-    params = {}
-    for k, v in params_parsed.iteritems():
-      if k in ('self', 'context'):
-        continue
-      if k.startswith('_'):
-        continue
-      params[k] = v
+    def process_request(self, request):
+        params_parsed = request.params
+        params = {}
+        for k, v in params_parsed.iteritems():
+            if k in ('self', 'context'):
+                continue
+            if k.startswith('_'):
+                continue
+            params[k] = v
 
-    request.environ[PARAMS_ENV] = params
+        request.environ[PARAMS_ENV] = params
 
 
 class JsonBodyMiddleware(wsgi.Middleware):
-  """Middleware to allow method arguments to be passed as serialized JSON.
+    """Middleware to allow method arguments to be passed as serialized JSON.
 
-  Accepting arguments as JSON is useful for accepting data that may be more
-  complex than simple primitives.
+    Accepting arguments as JSON is useful for accepting data that may be more
+    complex than simple primitives.
 
-  In this case we accept it as urlencoded data under the key 'json' as in
-  json=<urlencoded_json> but this could be extended to accept raw JSON
-  in the POST body.
+    In this case we accept it as urlencoded data under the key 'json' as in
+    json=<urlencoded_json> but this could be extended to accept raw JSON
+    in the POST body.
 
-  Filters out the parameters `self`, `context` and anything beginning with
-  an underscore.
+    Filters out the parameters `self`, `context` and anything beginning with
+    an underscore.
 
-  """
+    """
 
-  def process_request(self, request):
-    #if 'json' not in request.params:
-    #    return
+    def process_request(self, request):
+        #if 'json' not in request.params:
+        #    return
 
-    params_json = request.body
-    if not params_json:
-      return
+        params_json = request.body
+        if not params_json:
+            return
 
-    params_parsed = json.loads(params_json)
-    params = {}
-    for k, v in params_parsed.iteritems():
-      if k in ('self', 'context'):
-        continue
-      if k.startswith('_'):
-        continue
-      params[k] = v
+        params_parsed = json.loads(params_json)
+        params = {}
+        for k, v in params_parsed.iteritems():
+            if k in ('self', 'context'):
+                continue
+            if k.startswith('_'):
+                continue
+            params[k] = v
 
-    request.environ[PARAMS_ENV] = params
+        request.environ[PARAMS_ENV] = params
