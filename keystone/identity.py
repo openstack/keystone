bf7e6fb47d8587e4c5176c5281fb3bbe724a85a4
termie <github@anarkystic.com>
2012-01-18 10:54:48 -0800

    some tiny docs

--- keystone/identity.py
+++ keystone/identity.py
@@ -1,5 +1,7 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
+"""Main entry point into the Identity service."""
+
 from keystone import config
 from keystone import manager
 
@@ -8,5 +10,9 @@ CONF = config.CONF
 
 
 class Manager(manager.Manager):
+    """Default pivot point for the Identity backend.
+
+    See :mod:`keystone.manager.Manager` for more details.
+    """
     def __init__(self):
         super(Manager, self).__init__(CONF.identity.driver)
