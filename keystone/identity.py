f0e3e7f123c24f4441de14be34e5b9fd307354aa
termie <github@anarkystic.com>
2012-01-18 11:40:36 -0800

    add docs for various service managers

--- keystone/identity.py
+++ keystone/identity.py
@@ -12,7 +12,13 @@ CONF = config.CONF
 class Manager(manager.Manager):
     """Default pivot point for the Identity backend.
 
-    See :mod:`keystone.manager.Manager` for more details.
+    See :mod:`keystone.manager.Manager` for more details on how this
+    dynamically calls the backend.
+
+    See :mod:`keystone.backends.base.Identity` for more details on the
+    interface provided by backends.
+
     """
+
     def __init__(self):
         super(Manager, self).__init__(CONF.identity.driver)
--- keystone/token.py
+++ keystone/token.py
@@ -1,5 +1,7 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
+"""Main entry point into the Token service."""
+
 from keystone import config
 from keystone import manager
 
@@ -8,5 +10,15 @@ CONF = config.CONF
 
 
 class Manager(manager.Manager):
+    """Default pivot point for the Token backend.
+
+    See :mod:`keystone.manager.Manager` for more details on how this
+    dynamically calls the backend.
+
+    See :mod:`keystone.backends.base.Token` for more details on the
+    interface provided by backends.
+
+    """
+
     def __init__(self):
         super(Manager, self).__init__(CONF.token.driver)
