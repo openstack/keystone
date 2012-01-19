ff6af1f808bc962c816528174096b5e386c1cb9a
termie <github@anarkystic.com>
2012-01-18 21:09:04 -0800

    split up sql backends too

--- keystone/common/sql/migrate_repo/versions/001_add_initial_tables.py
+++ keystone/common/sql/migrate_repo/versions/001_add_initial_tables.py
@@ -1,13 +1,17 @@
 from sqlalchemy import *
 from migrate import *
 
-from keystone.backends import sql
+from keystone.common import sql
+
+# these are to make sure all the models we care about are defined
+import keystone.identity.backends.sql
+import keystone.contrib.ec2.backends.sql
 
 
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
-    sql.Base.metadata.create_all(migrate_engine)
+    sql.ModelBase.metadata.create_all(migrate_engine)
 
 
 def downgrade(migrate_engine):
