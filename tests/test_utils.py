95280608fc82f7ae51f8c42678d6a7f0ac2ecff0
Justin Santa Barbara <justin@fathomdb.com>
2012-02-08 12:53:44 -0800

    Cope with unicode passwords or None
    If the password has a unicode character in it, bcrypt breaks.  So encode it using utf-8.  utf-8 should mean that existing hashes still work.
    
    Change-Id: I4f9f3b636c8728234ada87de62d22bed2ff8eb60

---
+++ tests/test_utils.py
@@ -0,0 +1,40 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 Justin Santa Barbara
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from keystone import test
+from keystone.common import utils
+
+
+class UtilsTestCase(test.TestCase):
+    def test_hash(self):
+        password = 'right'
+        wrong = 'wrongwrong'  # Two wrongs don't make a right
+        hashed = utils.hash_password(password)
+        self.assertTrue(utils.check_password(password, hashed))
+        self.assertFalse(utils.check_password(wrong, hashed))
+
+    def test_hash_edge_cases(self):
+        hashed = utils.hash_password('secret')
+        self.assertFalse(utils.check_password('', hashed))
+        self.assertFalse(utils.check_password(None, hashed))
+
+    def test_hash_unicode(self):
+        password = u'Comment \xe7a va'
+        wrong = 'Comment ?a va'
+        hashed = utils.hash_password(password)
+        self.assertTrue(utils.check_password(password, hashed))
+        self.assertFalse(utils.check_password(wrong, hashed))
