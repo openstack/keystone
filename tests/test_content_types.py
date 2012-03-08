5c6bccf1c57b06a19845c696c19274ae9f080104
Michael Basnight <mbasnight@gmail.com>
2012-03-07 22:32:23 -0600

    fixes lp#949648 change belongsTo validate to name
    Change-Id: I3d36290ad95a0440c006e2daff5b831be62957ae

--- tests/test_content_types.py
+++ tests/test_content_types.py
@@ -355,7 +355,7 @@ class CoreApiTests(object):
     def test_validate_token_belongs_to(self):
         token = self.get_scoped_token()
         path = ('/v2.0/tokens/%s?belongs_to=%s'
-                 % (token, self.tenant_bar['id']))
+                 % (token, self.tenant_bar['name']))
         r = self.admin_request(path=path,token=token)
         self.assertValidAuthenticationResponse(r,
                                                require_service_catalog=True)
