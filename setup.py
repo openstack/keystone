b0bbd8a474589d39b46c83b767dd08489ec6024c
Thierry Carrez <thierry@openstack.org>
2012-09-28 16:09:13 +0200

    Bump next version to 2012.2.1
    Open stable/folsom by bumping next version to 2012.2.1.
    
    Change-Id: I9a705c14ac821cca48cb769d5c6cd9f7ae760f6f

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ depend_links = parse_dependency_links()
 write_requirements()
 
 setup(name='keystone',
-      version='2012.2',
+      version='2012.2.1',
       description="Authentication service for OpenStack",
       license='Apache License (2.0)',
       author='OpenStack, LLC.',
