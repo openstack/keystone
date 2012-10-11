ddb40198c9323ff8dc82a44a72e456a7bfe736b8
Mark McLoughlin <markmc@redhat.com>
2012-10-11 20:44:32 +0100

    Open 2012.1.4 development
    Bump version to 2012.1.4 to formally open development after the release
    of 2012.1.3.
    
    See http://wiki.openstack.org/StableBranchRelease
    
    Note - 2012.1.3 is expected to be the final official release of Essex.
    
    Change-Id: I0de6fae1495deab60bd667e4653210b22b994b39

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ depend_links = parse_dependency_links()
 write_requirements()
 
 setup(name='keystone',
-      version='2012.1.3',
+      version='2012.1.4',
       description="Authentication service for OpenStack",
       license='Apache License (2.0)',
       author='OpenStack, LLC.',
