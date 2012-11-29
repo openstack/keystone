7869c3ecd7b1ff72bfe817e97024fe1c7673fba4
Mark McLoughlin <markmc@redhat.com>
2012-11-29 21:29:23 +0000

    Bump next version to 2012.2.2
    Change-Id: I3b1ab6c059d152b59a18d7f7e3c799258e05ec68

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ depend_links = parse_dependency_links()
 write_requirements()
 
 setup(name='keystone',
-      version='2012.2.1',
+      version='2012.2.2',
       description="Authentication service for OpenStack",
       license='Apache License (2.0)',
       author='OpenStack, LLC.',
