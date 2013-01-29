2ab990ba43bf42494cce978bab0b75b32d2eb7dc
Matthew Treinish <treinish@linux.vnet.ibm.com>
2013-01-29 10:21:46 -0500

    Use install_venv_common.py from oslo.
    This syncs install_venv_common.py from oslo and reworks the
    tools/install_venv.py script to use the new library.
    
    Change-Id: I68fe3449ed0fcc7fdfb84af2dcc77a39ebde7f01

--- tools/install_venv.py
+++ tools/install_venv.py
@@ -1,5 +1,6 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
+# Copyright 2013 IBM Corp.
 # Copyright 2012 OpenStack LLC
 # Copyright 2010 United States Government as represented by the
 # Administrator of the National Aeronautics and Space Administration.
@@ -23,118 +24,52 @@ virtualenv installation script
 """
 
 import os
-import subprocess
 import sys
 
-
-ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-VENV = os.path.join(ROOT, '.venv')
-PIP_REQUIRES = os.path.join(ROOT, 'tools', 'pip-requires')
-TEST_REQUIRES = os.path.join(ROOT, 'tools', 'test-requires')
-PY_VERSION = "python%s.%s" % (sys.version_info[0], sys.version_info[1])
-
-
-def die(message, *args):
-    print >>sys.stderr, message % args
-    sys.exit(1)
-
-
-def check_python_version():
-    if sys.version_info < (2, 6):
-        die("Need Python Version >= 2.6")
-
-
-def run_command(cmd, redirect_output=True, check_exit_code=True):
-    """
-    Runs a command in an out-of-process shell, returning the
-    output of that command.  Working directory is ROOT.
-    """
-    if redirect_output:
-        stdout = subprocess.PIPE
-    else:
-        stdout = None
-
-    proc = subprocess.Popen(cmd, cwd=ROOT, stdout=stdout)
-    output = proc.communicate()[0]
-    if check_exit_code and proc.returncode != 0:
-        die('Command "%s" failed.\n%s', ' '.join(cmd), output)
-    return output
-
-
-HAS_EASY_INSTALL = bool(run_command(['which', 'easy_install'],
-                        check_exit_code=False).strip())
-HAS_VIRTUALENV = bool(run_command(['which', 'virtualenv'],
-                      check_exit_code=False).strip())
-
-
-def check_dependencies():
-    """Make sure virtualenv is in the path."""
-
-    print 'Checking for virtualenv...'
-    if not HAS_VIRTUALENV:
-        print 'not found.'
-        # Try installing it via easy_install...
-        if HAS_EASY_INSTALL:
-            print 'Installing virtualenv via easy_install...',
-            if not (run_command(['which', 'easy_install']) and
-                    run_command(['easy_install', 'virtualenv'])):
-                die('ERROR: virtualenv not found.\n\nNova development'
-                    ' requires virtualenv, please install it using your'
-                    ' favorite package management tool')
-            print 'done.'
-    print 'done.'
-
-
-def create_virtualenv(venv=VENV):
-    """Creates the virtual environment and installs PIP only into the
-    virtual environment
-    """
-    print 'Creating venv...',
-    run_command(['virtualenv', '-q', '--no-site-packages', VENV])
-    print 'done.'
-    print 'Installing pip in virtualenv...',
-    if not run_command(['tools/with_venv.sh', 'easy_install', 'pip']).strip():
-        die("Failed to install pip.")
-    print 'done.'
-
-
-def install_dependencies(venv=VENV):
-    print 'Installing dependencies with pip (this can take a while)...'
-    run_command(['tools/with_venv.sh', 'pip', 'install', '-r',
-                PIP_REQUIRES, '-r', TEST_REQUIRES], redirect_output=False)
-
-
-def install_self():
-    print 'Installing project code into virtualenv so that paths work'
-    run_command(['tools/with_venv.sh', 'python', 'setup.py', 'develop'],
-                redirect_output=True)
+import install_venv_common as install_venv
 
 
 def print_help():
     help = """
-    Virtual environment configuration complete.
+    Keystone development environment setup is complete.
 
-    To activate the virtualenv for the extent of your current shell
+    Keystone development uses virtualenv to track and manage Python
+    dependencies while in development and testing.
+
+    To activate the Keystone virtualenv for the extent of your current shell
     session you can run:
 
-    $ source %s/bin/activate
+    $ source .venv/bin/activate
 
     Or, if you prefer, you can run commands in the virtualenv on a case by case
     basis by running:
 
     $ tools/with_venv.sh <your command>
 
-    """ % VENV
+    Also, make test will automatically use the virtualenv.
+    """
     print help
 
 
 def main(argv):
-    check_python_version()
-    check_dependencies()
-    create_virtualenv()
-    install_dependencies()
-    install_self()
+    root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    venv = os.path.join(root, '.venv')
+    pip_requires = os.path.join(root, 'tools', 'pip-requires')
+    test_requires = os.path.join(root, 'tools', 'test-requires')
+    py_version = "python%s.%s" % (sys.version_info[0], sys.version_info[1])
+    project = 'Keystone'
+    install = install_venv.InstallVenv(root, venv, pip_requires, test_requires,
+                                       py_version, project)
+    options = install.parse_args(argv)
+    install.check_python_version()
+    install.check_dependencies()
+    install.create_virtualenv(no_site_packages=options.no_site_packages)
+    install.install_dependencies()
+    install.run_command([os.path.join(venv, 'bin/python'),
+                        'setup.py', 'develop'])
+    install.post_process()
     print_help()
+
 
 if __name__ == '__main__':
     main(sys.argv)
