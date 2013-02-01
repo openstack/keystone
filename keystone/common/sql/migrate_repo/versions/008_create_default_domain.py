openstack/keystone

commit 5bc46d861e9c8f355d2bdd68912be3e64c2dc9e9

Create a default domain (bp default-domain)
This changes rewrites some of our migration history since the folsom
release so that we can create a default domain prior to creating
non-nullable foreignkey's in the user and project tables in migration
9 (numbered according to this change).

DocImpact

Change-Id: I807f7b1dca1d6a895f7417c316bcbce24ada61c0

keystone/common/sql/migrate_repo/versions/008_create_default_domain.py
@@ -1,0 +1,63 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 OpenStack LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+
+import sqlalchemy as sql
+from sqlalchemy import orm
+from keystone import config
+
+
+CONF = config.CONF
+DEFAULT_DOMAIN_ID = CONF['identity']['default_domain_id']
+
+
+def upgrade(migrate_engine):
+    """Creates the default domain."""
+    meta = sql.MetaData()
+    meta.bind = migrate_engine
+
+    domain_table = sql.Table('domain', meta, autoload=True)
+
+    domain = {
+        'id': DEFAULT_DOMAIN_ID,
+        'name': 'Default',
+        'enabled': True,
+        'extra': json.dumps({
+            'description': 'Owns users and tenants (i.e. projects) available '
+                           'on Identity API v2.'})}
+
+    session = orm.sessionmaker(bind=migrate_engine)()
+
+    session.execute(
+        'INSERT INTO `%s` (%s) VALUES (%s)' % (
+            domain_table.name,
+            ', '.join(['`%s`' % k for k in domain.keys()]),
+            ', '.join([':%s' % k for k in domain.keys()])),
+        domain)
+    session.commit()
+
+
+def downgrade(migrate_engine):
+    """Delete the default domain."""
+    meta = sql.MetaData()
+    meta.bind = migrate_engine
+
+    sql.Table('domain', meta, autoload=True)
+    session = orm.sessionmaker(bind=migrate_engine)()
+    session.execute(
+        'DELETE FROM `domain` WHERE `id`=:id', {'id': DEFAULT_DOMAIN_ID})
+    session.commit()

etc/keystone.conf.sample
@@ -76,6 +76,14 @@
 [identity]
 # driver = keystone.identity.backends.sql.Identity
 
+# This references the domain to use for all Identity API v2 requests (which are
+# not aware of domains). A domain with this ID will be created for you by
+# keystone-manage db_sync in migration 008.  The domain referenced by this ID
+# cannot be deleted on the v3 API, to prevent accidentally breaking the v2 API.
+# There is nothing special about this domain, other than the fact that it must
+# exist to order to maintain support for your v2 clients.
+# default_domain_id = default
+
 [catalog]
 # dynamic, sql-based backend (supports API/CLI-based management commands)
 # driver = keystone.catalog.backends.sql.Catalog

commit 4722c84fb90c51fb5810ad7b46c48230ecee1a6c

Updates migration 008 to work on PostgreSQL.
This fixes a regression in 5bc46d8 which caused migration 008 to
fail when using PostgreSQL.

Fixes LP Bug #1112535.

Change-Id: I94f69c2dece3a5753eed3cc3cf7b63ba60208c5c

keystone/common/sql/migrate_repo/versions/008_create_default_domain.py
@@ -41,13 +41,8 @@ def upgrade(migrate_engine):
                            'on Identity API v2.'})}
 
     session = orm.sessionmaker(bind=migrate_engine)()
-
-    session.execute(
-        'INSERT INTO `%s` (%s) VALUES (%s)' % (
-            domain_table.name,
-            ', '.join(['`%s`' % k for k in domain.keys()]),
-            ', '.join([':%s' % k for k in domain.keys()])),
-        domain)
+    insert = domain_table.insert()
+    insert.execute(domain)
     session.commit()
 
 
@@ -59,5 +54,5 @@ def downgrade(migrate_engine):
     sql.Table('domain', meta, autoload=True)
     session = orm.sessionmaker(bind=migrate_engine)()
     session.execute(
-        'DELETE FROM `domain` WHERE `id`=:id', {'id': DEFAULT_DOMAIN_ID})
+        'DELETE FROM domain WHERE id=:id', {'id': DEFAULT_DOMAIN_ID})
     session.commit()