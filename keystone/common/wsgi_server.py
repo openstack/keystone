489677319b416351d73044ae1336ff2a2112340e
Brant Knudson <bknudson@us.ibm.com>
2013-05-19 10:03:38 -0500

    Disable eventlet monkey-patching of DNS
    This change avoids eventlet's monkey-patching of DNS resolution.
    eventlet's doesn't support IPv6, for example.
    
    The way to avoid eventlet's DNS is to set an environment
    variable. The trick is the environment variable needs to be set
    before eventlet is imported.
    
    A similar change was made in nova, so this is just copying that
    code and technique to Keystone.
    
    This allows re-enabling the IPv6 tests, too.
    
    Change-Id: I1e4d4bbfea374d386796d1505a602fd187e75c67

--- keystone/common/wsgi_server.py
+++ keystone/common/wsgi_server.py
@@ -23,6 +23,19 @@ import socket
 import ssl
 import sys
 
+# NOTE(mikal): All of this is because if dnspython is present in your
+# environment then eventlet monkeypatches socket.getaddrinfo() with an
+# implementation which doesn't work for IPv6. What we're checking here is
+# that the magic environment variable was set when the import happened.
+if ('eventlet' in sys.modules and
+        os.environ.get('EVENTLET_NO_GREENDNS', '').lower() != 'yes'):
+    raise ImportError('eventlet imported before '
+                      'keystone.common.wsgi_server '
+                      '(EVENTLET_NO_GREENDNS env var set to %s)'
+                      % os.environ.get('EVENTLET_NO_GREENDNS'))
+
+os.environ['EVENTLET_NO_GREENDNS'] = 'yes'
+
 import eventlet
 import eventlet.wsgi
 
