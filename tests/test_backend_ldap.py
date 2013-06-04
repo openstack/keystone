c100fd2f1fe024cb2f731bfdd283cee36259e6e3
Jose Castro Leon <jose.castro.leon@cern.ch>
2013-06-04 11:59:35 -0400

    Force simple Bind for authentication
    The authentication code was using a common code path with
    other LDAP code that got an LDAP connection.  If the system
    was configured to do Anonymous binding, users could by pass
    the authentication check.
    
    This patch forces the authentication code to do a simple_bind.
    
    Change-Id: Id0c19f09d615446927db1ba074561b129329b5c8

--- tests/test_backend_ldap.py
+++ tests/test_backend_ldap.py
@@ -577,6 +577,26 @@ class LDAPIdentity(test.TestCase, test_backend.IdentityTests):
               'name': 'Default',
               'enabled': True}])
 
+    def test_authenticate_requires_simple_bind(self):
+        user = {
+            'id': 'no_meta',
+            'name': 'NO_META',
+            'domain_id': test_backend.DEFAULT_DOMAIN_ID,
+            'password': 'no_meta2',
+            'enabled': True,
+        }
+        self.identity_man.create_user({}, user['id'], user)
+        self.identity_api.add_user_to_project(self.tenant_baz['id'],
+                                              user['id'])
+        self.identity_api.user.LDAP_USER = None
+        self.identity_api.user.LDAP_PASSWORD = None
+
+        self.assertRaises(AssertionError,
+                          self.identity_api.authenticate,
+                          user_id=user['id'],
+                          tenant_id=self.tenant_baz['id'],
+                          password=None)
+
 
 class LDAPIdentityEnabledEmulation(LDAPIdentity):
     def setUp(self):
