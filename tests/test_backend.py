9559bdf1db33a6c49173f5b46e79af8099676ad5
Zhang Chun <zhang.chun.os@99cloud.net>
2013-07-05 00:05:17 +0800

    Remove an enumerate call
    Refactor tests/test_backend.py, remove a useless enumerate call in for
    loop.
    
    Change-Id: I1268ce70d40994078343ec3120021c131e52713d

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1065,7 +1065,7 @@ class IdentityTests(object):
             project_id=project1['id'])
 
         roles_ref_ids = []
-        for i, ref in enumerate(roles_ref):
+        for ref in roles_ref:
             roles_ref_ids.append(ref['id'])
         self.assertIn(role1['id'], roles_ref_ids)
         self.assertIn(role2['id'], roles_ref_ids)
