4bfa58ddb80c8484ad510d65fd3760429e5f3c6c
YangLei <yanglyy@cn.ibm.com>
2013-09-04 11:14:22 +0800

    Need to use _() to handle i18n string messages
    Partial-Bug: #1220502
    Change-Id: If31dd659794743837e2e494a2b6ae4488ef6ca32

--- keystone/identity/controllers.py
+++ keystone/identity/controllers.py
@@ -92,7 +92,7 @@ class Tenant(controller.V2Controller):
         tenant_ref = self._normalize_dict(tenant)
 
         if 'name' not in tenant_ref or not tenant_ref['name']:
-            msg = 'Name field is required and cannot be empty'
+            msg = _('Name field is required and cannot be empty')
             raise exception.ValidationError(message=msg)
 
         self.assert_admin(context)
@@ -141,7 +141,7 @@ class Tenant(controller.V2Controller):
                     first_index = marker_index + 1
                     break
             else:
-                msg = 'Marker could not be found'
+                msg = _('Marker could not be found')
                 raise exception.ValidationError(message=msg)
 
         limit = kwargs.get('limit')
@@ -152,7 +152,7 @@ class Tenant(controller.V2Controller):
                 if limit < 0:
                     raise AssertionError()
             except (ValueError, AssertionError):
-                msg = 'Invalid limit value'
+                msg = _('Invalid limit value')
                 raise exception.ValidationError(message=msg)
             last_index = first_index + limit
 
@@ -197,10 +197,10 @@ class User(controller.V2Controller):
         self.assert_admin(context)
 
         if 'name' not in user or not user['name']:
-            msg = 'Name field is required and cannot be empty'
+            msg = _('Name field is required and cannot be empty')
             raise exception.ValidationError(message=msg)
         if 'enabled' in user and not isinstance(user['enabled'], bool):
-            msg = 'Enabled field must be a boolean'
+            msg = _('Enabled field must be a boolean')
             raise exception.ValidationError(message=msg)
 
         default_tenant_id = user.get('tenantId', None)
@@ -220,7 +220,7 @@ class User(controller.V2Controller):
         self.assert_admin(context)
 
         if 'enabled' in user and not isinstance(user['enabled'], bool):
-            msg = 'Enabled field should be a boolean'
+            msg = _('Enabled field should be a boolean')
             raise exception.ValidationError(message=msg)
 
         user_ref = self.identity_api.update_user(user_id, user)
@@ -285,7 +285,7 @@ class Role(controller.V2Controller):
         self.assert_admin(context)
 
         if 'name' not in role or not role['name']:
-            msg = 'Name field is required and cannot be empty'
+            msg = _('Name field is required and cannot be empty')
             raise exception.ValidationError(message=msg)
 
         role_id = uuid.uuid4().hex
@@ -816,12 +816,12 @@ class RoleV3(controller.V3Controller):
 
     def _require_domain_xor_project(self, domain_id, project_id):
         if (domain_id and project_id) or (not domain_id and not project_id):
-            msg = 'Specify a domain or project, not both'
+            msg = _('Specify a domain or project, not both')
             raise exception.ValidationError(msg)
 
     def _require_user_xor_group(self, user_id, group_id):
         if (user_id and group_id) or (not user_id and not group_id):
-            msg = 'Specify a user or group, not both'
+            msg = _('Specify a user or group, not both')
             raise exception.ValidationError(msg)
 
     def _check_if_inherited(self, context):
