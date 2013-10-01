c3b92295b718a41c3136876eb39297081015a97c
Matt Odden <mrodden@us.ibm.com>
2013-10-01 08:52:15 +0000

    Disable lazy gettext
    This change disables lazy gettext functionality in Keystone,
    due to problems with character encoding and logging
    detailed in bug 1225099.
    
    It is part of a series of commits across all projects with
    lazy gettext enabled.
    
    Change-Id: Ia934a7df9386baf6ae8eb9ff48c24386c47ecd23
    Partial-bug: 1225099

--- httpd/keystone.py
+++ httpd/keystone.py
@@ -24,11 +24,7 @@ from keystone.openstack.common import gettextutils
 # NOTE(blk-u):
 # gettextutils.install() must run to set _ before importing any modules that
 # contain static translated strings.
-#
-# Configure gettextutils for deferred translation of messages
-# so that error messages in responses can be translated according to the
-# Accept-Language in the request rather than the Keystone server locale.
-gettextutils.install('keystone', lazy=True)
+gettextutils.install('keystone')
 
 from keystone.common import environment
 from keystone import config
