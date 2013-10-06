8285cd5065bd906ce9fc800f219742aef27af964
Doug Hellmann <doug.hellmann@dreamhost.com>
2013-10-06 12:47:04 -0400

    Use abstract base class for auth handler
    Use the abc module for managing abstract base classes in the driver
    layer.
    
    bp abstract-base-class-drivers
    
    Change-Id: I302a9bf0ac3aea2a2f687e0ceda7e107c0c11539

--- keystone/auth/core.py
+++ keystone/auth/core.py
@@ -14,17 +14,23 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import abc
+
+import six
+
 from keystone.common import dependency
 from keystone import exception
 
 
 @dependency.requires('identity_api')
+@six.add_metaclass(abc.ABCMeta)
 class AuthMethodHandler(object):
     """Abstract base class for an authentication plugin."""
 
     def __init__(self):
         pass
 
+    @abc.abstractmethod
     def authenticate(self, context, auth_payload, auth_context):
         """Authenticate user and return an authentication context.
 
