82dcde08f60c45002955875664a3cf82d1d211bc
Brant Knudson <bknudson@us.ibm.com>
2013-10-21 15:21:12 -0500

    Fix remove role assignment adds role using LDAP assignment
    When using the LDAP assignment backend, attempting to remove a
    role assignment when the role hadn't been used before would
    actually add the role assignment and would not return a
    404 Not Found like the SQL backend.
    
    This change makes it so that when attempt to remove a role that
    wasn't assigned then 404 Not Found is returned.
    
    Closes-Bug: #1242855
    Change-Id: I28ccd26cc4bb1a241d0363d0ab52d2c11410e8b3
    (cherry picked from commit c6800ca1ac984c879e75826df6694d6199444ea0)
    (cherry picked from commit b17e7bec768bd53d3977352486378698a3db3cfa)
    (cherry picked from commit 4221b6020e6b0b42325d8904d7b8a22577a6acc0)

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -57,6 +57,15 @@ class IdentityTests(object):
         user_refs = self.identity_api.get_project_users(self.tenant_bar['id'])
         self.assertNotIn(self.user_two['id'], [x['id'] for x in user_refs])
 
+    def test_remove_user_role_not_assigned(self):
+        # Expect failure if attempt to remove a role that was never assigned to
+        # the user.
+        self.assertRaises(exception.RoleNotFound,
+                          self.identity_api.remove_role_from_user_and_project,
+                          tenant_id=self.tenant_bar['id'],
+                          user_id=self.user_two['id'],
+                          role_id=self.role_other['id'])
+
     def test_authenticate_bad_user(self):
         self.assertRaises(AssertionError,
                           self.identity_api.authenticate,
