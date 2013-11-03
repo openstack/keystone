8ae6f39f46e8bb11d28535609daf8ba7947db521
David Stanek <dstanek@dstanek.com>
2013-11-03 16:26:36 +0000

    Rewrites the serveapp method into a fixture
    As a fixture we will no longer have to remember to kill the server when
    you are done with it. Additionally we are able to use the fixture as a
    context manager.
    
    Change-Id: If283e12cde362d3eadc96318b5cf6799a813c14a
    Partial-Bug: #1247443

---
+++ keystone/tests/fixtures/appserver.py
@@ -0,0 +1,83 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+import fixtures
+from paste import deploy
+
+from keystone.common import environment
+from keystone import config
+
+
+CONF = config.CONF
+
+environment.use_eventlet()
+
+MAIN = 'main'
+ADMIN = 'admin'
+
+
+class AppServer(fixtures.Fixture):
+    """A fixture for managing an application server instance.
+    """
+
+    def __init__(self, config, name, cert=None, key=None, ca=None,
+                 cert_required=False, host='127.0.0.1', port=0):
+        super(AppServer, self).__init__()
+        self.config = config
+        self.name = name
+        self.cert = cert
+        self.key = key
+        self.ca = ca
+        self.cert_required = cert_required
+        self.host = host
+        self.port = port
+
+    def setUp(self):
+        super(AppServer, self).setUp()
+
+        app = deploy.loadapp(self.config, name=self.name)
+        self.server = environment.Server(app, self.host, self.port)
+        self._setup_SSL_if_requested()
+        self.server.start(key='socket')
+
+        # some tests need to know the port we ran on.
+        self.port = self.server.socket_info['socket'][1]
+        self._update_config_opt()
+
+        self.addCleanup(self.server.kill)
+
+    def _setup_SSL_if_requested(self):
+        # TODO(dstanek): fix environment.Server to take a SSLOpts instance
+        # so that the params are either always set or not
+        if (self.cert is not None and
+                self.ca is not None and
+                self.key is not None):
+            self.server.set_ssl(certfile=self.cert,
+                                keyfile=self.key,
+                                ca_certs=self.ca,
+                                cert_required=self.cert_required)
+
+    def _update_config_opt(self):
+        """Updates the config with the actual port used."""
+        opt_name = self._get_config_option_for_section_name()
+        CONF.set_override(opt_name, self.port)
+
+    def _get_config_option_for_section_name(self):
+        """Maps Paster config section names to port option names."""
+        return {'admin': 'admin_port', 'main': 'public_port'}[self.name]
--- keystone/tests/test_ipv6.py
+++ keystone/tests/test_ipv6.py
@@ -18,6 +18,7 @@
 from keystone.common import environment
 from keystone import config
 from keystone import tests
+from keystone.tests.fixtures import appserver
 
 
 CONF = config.CONF
@@ -32,17 +33,18 @@ class IPv6TestCase(tests.TestCase):
 
     def test_ipv6_ok(self):
         """Make sure both public and admin API work with ipv6."""
-        self.public_server = self.serveapp('keystone', name='main',
-                                           host="::1", port=0)
-        self.admin_server = self.serveapp('keystone', name='admin',
-                                          host="::1", port=0)
+        paste_conf = self._paste_config('keystone')
+
         # Verify Admin
-        conn = environment.httplib.HTTPConnection('::1', CONF.admin_port)
-        conn.request('GET', '/')
-        resp = conn.getresponse()
-        self.assertEqual(resp.status, 300)
+        with appserver.AppServer(paste_conf, appserver.ADMIN, host="::1"):
+            conn = environment.httplib.HTTPConnection('::1', CONF.admin_port)
+            conn.request('GET', '/')
+            resp = conn.getresponse()
+            self.assertEqual(resp.status, 300)
+
         # Verify Public
-        conn = environment.httplib.HTTPConnection('::1', CONF.public_port)
-        conn.request('GET', '/')
-        resp = conn.getresponse()
-        self.assertEqual(resp.status, 300)
+        with appserver.AppServer(paste_conf, appserver.MAIN, host="::1"):
+            conn = environment.httplib.HTTPConnection('::1', CONF.public_port)
+            conn.request('GET', '/')
+            resp = conn.getresponse()
+            self.assertEqual(resp.status, 300)
