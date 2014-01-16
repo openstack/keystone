a2e6f9c03a065d23ef8b48282240655f81c6c351
Dolph Mathews <dolph.mathews@gmail.com>
2014-01-16 15:58:34 -0600

    remove unused LOG
    Change-Id: I5dec1b23667c7226697f064fe793c556c71e3abe

--- keystone/auth/plugins/external.py
+++ keystone/auth/plugins/external.py
@@ -23,11 +23,8 @@ import six
 from keystone import auth
 from keystone.common import config
 from keystone import exception
-from keystone.openstack.common import log
 from keystone.openstack.common import versionutils
 
-
-LOG = log.getLogger(__name__)
 
 CONF = config.CONF
 
