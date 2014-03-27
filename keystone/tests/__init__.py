a20158766b71964588deccea46b1431dcfe5c4fe
David Stanek <dstanek@dstanek.com>
2014-03-27 22:47:42 +0000

    First real Python 3 tests
    The goal of this changeset is to enable at least some of the tests to be
    run under Python3. Even if we can't get them all to run initially it's
    important that we start seeing the changes from bp keystone-py3kcompat.
    Also it's important that we are checking for regressions. If we do all
    this Python 3 work, but don't have automated tests in place we may
    inadvertantly undo that work.
    
    In this initial changeset I had to do some not nice things to get this
    to work.
    
    First I had to switch to using the nose test runner. testr seems to be
    much more aggressive about importing everything, even when you are not
    going to run the test module. This idea came from oslo. Once we have
    full Python 3 support we can go back to testr.
    
    Then I had to mock out some of the libraries that don't work on Python
    3. This is unfortunate, but had to be done because keystone.test.core
    indirectly imports a significant amount of the codebase.
    
    bp python3
    
    Change-Id: I6dd0d1b84380c4c386f70b72cdfcdf9b744c755f

--- keystone/tests/__init__.py
+++ keystone/tests/__init__.py
@@ -12,6 +12,31 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import six
+
+
+if six.PY3:
+    # NOTE(dstanek): This block will monkey patch libraries that are not
+    # yet supported in Python3. We do this that that it is possible to
+    # execute any tests at all. Without monkey patching modules the
+    # tests will fail with import errors.
+
+    import sys
+    from unittest import mock  # noqa: our import detection is naive?
+
+    # NOTE(dstanek): oslo.config is imported there so that later module
+    # that use it will still have access. If we don't import it now then
+    # it won't be available because of how I am patching oslo below.
+    import oslo.config  # noqa: need this imported before the monkey patching
+    sys.modules['eventlet'] = mock.Mock()
+    sys.modules['eventlet.green'] = mock.Mock()
+    sys.modules['eventlet.wsgi'] = mock.Mock()
+    sys.modules['oslo'] = mock.Mock()
+    sys.modules['oslo.messaging'] = mock.Mock()
+    sys.modules['pycadf'] = mock.Mock()
+    sys.modules['paste'] = mock.Mock()
+
+
 # NOTE(dstanek): gettextutils.enable_lazy() must be called before
 # gettextutils._() is called to ensure it has the desired lazy lookup
 # behavior. This includes cases, like keystone.exceptions, where
