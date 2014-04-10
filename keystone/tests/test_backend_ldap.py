1716748df15443f1141f69ea620c4bc33758433d
Nathan Kinder <nkinder@redhat.com>
2014-04-09 18:24:21 -0700

    Treat LDAP attribute names as case-insensitive
    LDAP attribute names are case-insensitive, and different LDAP
    server implementations behave differently when returning search
    results.  Keystone attempts to use the attribute names as defined
    in the mapping configuration when checking for values in the dict
    that represents the search results.  If the case doesn't match,
    we won't find the values which can lead to errors.
    
    This patch converts the dictionary keys to lowercase to allow us
    to perform a case-insensitive lookup when processing LDAP search
    results.  When we create the model object, we still use the
    attribute name case as defined in the mapping to allow predictable
    lookups when we process the model object later.
    
    Change-Id: Ib9a03b912bc7652dc6a61b1c97a950e263a9c6fa
    Closes-bug: 1281216
    (cherry picked from commit 7c104e13fce919dd2563f06ce31c059f145beb77)

--- keystone/tests/test_backend_ldap.py
+++ keystone/tests/test_backend_ldap.py
@@ -17,10 +17,12 @@ import copy
 import uuid
 
 import ldap
+import mock
 
 from keystone import assignment
 from keystone.common import cache
 from keystone.common import ldap as common_ldap
+from keystone.common.ldap import core as common_ldap_core
 from keystone.common import sql
 from keystone import config
 from keystone import exception
@@ -983,6 +985,23 @@ class LDAPIdentity(BaseLDAPIdentity, tests.TestCase):
         self.identity_api.create_user(user['id'], user)
         dn, attrs = self.identity_api.driver.user._ldap_get(user['id'])
         self.assertTrue(user['name'] in attrs['description'])
+
+    @mock.patch.object(common_ldap_core.BaseLdap, '_ldap_get')
+    def test_user_mixed_case_attribute(self, mock_ldap_get):
+        # Mock the search results to return attribute names
+        # with unexpected case.
+        mock_ldap_get.return_value = (
+            'cn=junk,dc=example,dc=com',
+            {
+                'sN': [uuid.uuid4().hex],
+                'eMaIl': [uuid.uuid4().hex]
+            }
+        )
+        user = self.identity_api.get_user('junk')
+        self.assertEqual(mock_ldap_get.return_value[1]['sN'][0],
+                         user['name'])
+        self.assertEqual(mock_ldap_get.return_value[1]['eMaIl'][0],
+                         user['email'])
 
     def test_parse_extra_attribute_mapping(self):
         option_list = ['description:name', 'gecos:password',
