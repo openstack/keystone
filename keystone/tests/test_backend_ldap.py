c6cd627ea09e7d0dc40eee58ab2784353a71b2e0
Nathan Kinder <nkinder@redhat.com>
2014-06-28 08:05:42 -0700

    Implicitly ignore attributes that are mapped to None in LDAP
    Attributes that are mapped to None in LDAP trigger a 500 error when
    performing a search if they are not explicitly ignored in keystone's
    configuration.  These attributes should always be ignored, even if
    the admin left the attribute out of the ignore list.
    
    Change-Id: Ibbabdd0013059d5720250816764021a0b3ce8ce0
    Closes-bug: #1335437
    (cherry picked from commit 10a3edb7800d380ed72fcbbaae98251998c0e2c7)

--- keystone/tests/test_backend_ldap.py
+++ keystone/tests/test_backend_ldap.py
@@ -531,6 +531,26 @@ class BaseLDAPIdentity(test_backend.IdentityTests):
         # If this doesn't raise, then the test is successful.
         self.identity_api.create_user(u'fäké1', user)
 
+    def test_unignored_user_none_mapping(self):
+        # Ensure that an attribute that maps to None that is not explicitly
+        # ignored in configuration is implicitly ignored without triggering
+        # an error.
+        conf = self.get_config(CONF.identity.default_domain_id)
+        conf.ldap.user_attribute_ignore = ['enabled', 'email',
+                                           'tenants', 'tenantId']
+        self.reload_backends(CONF.identity.default_domain_id)
+
+        user = {'id': u'fäké1',
+                'name': u'fäké1',
+                'password': u'fäképass1',
+                'domain_id': CONF.identity.default_domain_id,
+                }
+
+        user_ref = self.identity_api.create_user(u'fäké1', user)
+
+        # If this doesn't raise, then the test is successful.
+        self.identity_api.get_user(user_ref['id'])
+
     def test_update_user_name(self):
         """A user's name cannot be changed through the LDAP driver."""
         self.assertRaises(exception.Conflict,
