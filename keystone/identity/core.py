27974d23f3df2538b6218f01cf64a679ecc16e78
Brant Knudson <bknudson@us.ibm.com>
2014-10-01 18:54:58 -0500

    Internal notifications for cleanup domain
    Use existing domain deleted notification for cleaning up domain
    contents in identity rather than cross-manager calls. This
    decreases coupling between managers.
    
    bp prefer-notifications
    
    Change-Id: I4adaeeddb50689b46dc4d832b0190748bbc18393

--- keystone/identity/core.py
+++ keystone/identity/core.py
@@ -270,6 +270,42 @@ class Manager(manager.Manager):
         super(Manager, self).__init__(CONF.identity.driver)
         self.domain_configs = DomainConfigs()
 
+        self.event_callbacks = {
+            notifications.ACTIONS.deleted: {
+                'domain': [self._domain_deleted],
+            },
+        }
+
+    def _domain_deleted(self, service, resource_type, operation,
+                        payload):
+        domain_id = payload['resource_info']
+
+        user_refs = self.list_users(domain_scope=domain_id)
+        group_refs = self.list_groups(domain_scope=domain_id)
+
+        for group in group_refs:
+            # Cleanup any existing groups.
+            if group['domain_id'] == domain_id:
+                try:
+                    self.delete_group(group['id'])
+                except exception.GroupNotFound:
+                    LOG.debug(('Group %(groupid)s not found when deleting '
+                               'domain contents for %(domainid)s, continuing '
+                               'with cleanup.'),
+                              {'groupid': group['id'], 'domainid': domain_id})
+
+        # And finally, delete the users themselves
+        for user in user_refs:
+            if user['domain_id'] == domain_id:
+                try:
+                    self.delete_user(user['id'])
+                except exception.UserNotFound:
+                    LOG.debug(('User %(userid)s not found when '
+                               'deleting domain contents for %(domainid)s, '
+                               'continuing with cleanup.'),
+                              {'userid': user['id'],
+                               'domainid': domain_id})
+
     # Domain ID normalization methods
 
     def _set_domain_id_and_mapping(self, ref, domain_id, driver,
