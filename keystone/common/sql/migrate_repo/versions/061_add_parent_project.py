529da7496babafad0ef8295bf6a5cc918c2bfefb
Brant Knudson <bknudson@us.ibm.com>
2015-01-08 08:37:10 -0600

    Fix downgrade from migration 61 on non-sqlite
    Downgrading from migration 61 on non-sqlite databases was failing
    with an AttributeError.
    
    Change-Id: Ia972c49d3f41853bdd3bccab9f839bd8b922f3e7
    Closes-Bug: #1408658

--- keystone/common/sql/migrate_repo/versions/061_add_parent_project.py
+++ keystone/common/sql/migrate_repo/versions/061_add_parent_project.py
@@ -44,10 +44,11 @@ def downgrade(migrate_engine):
     meta = sql.MetaData()
     meta.bind = migrate_engine
 
+    project_table = sql.Table(_PROJECT_TABLE_NAME, meta, autoload=True)
+
     # SQLite does not support constraints, and querying the constraints
     # raises an exception
     if migrate_engine.name != 'sqlite':
-        migration_helpers.remove_constraints(list_constraints(meta))
+        migration_helpers.remove_constraints(list_constraints(project_table))
 
-    project_table = sql.Table(_PROJECT_TABLE_NAME, meta, autoload=True)
     project_table.drop_column(_PARENT_ID_COLUMN_NAME)
