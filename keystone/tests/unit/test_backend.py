57c1c82a52d18ce324327dc6f6aa7f73c0bd9bb7
Henry Nash <henryn@linux.vnet.ibm.com>
2015-02-17 00:28:42 +0000

    Move backend role tests into their own module
    This is the first of a set of patches that break up the large
    backend.py test module into more maintainable chunks. This
    patch shows the structure proposed and the first piece
    broken out (all the SQL tests for role management). A follow on
    patch will do the same for the LDAP role management tests, and then
    subsequent patches will split our resource, assignment, identity etc.
    
    Partially Implements: blueprint backends-tests-restructuration
    
    Change-Id: I0b24e2511f318e69a1a20f07ff3388f7a1975440

--- keystone/tests/unit/test_backend.py
+++ keystone/tests/unit/test_backend.py
@@ -246,43 +246,6 @@ class IdentityTests(object):
                           uuid.uuid4().hex,
                           DEFAULT_DOMAIN_ID)
 
-    def test_get_role(self):
-        role_ref = self.role_api.get_role(self.role_admin['id'])
-        role_ref_dict = dict((x, role_ref[x]) for x in role_ref)
-        self.assertDictEqual(role_ref_dict, self.role_admin)
-
-    def test_get_role_404(self):
-        self.assertRaises(exception.RoleNotFound,
-                          self.role_api.get_role,
-                          uuid.uuid4().hex)
-
-    def test_create_duplicate_role_name_fails(self):
-        role = {'id': 'fake1',
-                'name': 'fake1name'}
-        self.role_api.create_role('fake1', role)
-        role['id'] = 'fake2'
-        self.assertRaises(exception.Conflict,
-                          self.role_api.create_role,
-                          'fake2',
-                          role)
-
-    def test_rename_duplicate_role_name_fails(self):
-        role1 = {
-            'id': 'fake1',
-            'name': 'fake1name'
-        }
-        role2 = {
-            'id': 'fake2',
-            'name': 'fake2name'
-        }
-        self.role_api.create_role('fake1', role1)
-        self.role_api.create_role('fake2', role2)
-        role1['name'] = 'fake2name'
-        self.assertRaises(exception.Conflict,
-                          self.role_api.update_role,
-                          'fake1',
-                          role1)
-
     def test_create_duplicate_user_name_fails(self):
         user = {'name': 'fake1',
                 'domain_id': DEFAULT_DOMAIN_ID,
@@ -1623,32 +1586,6 @@ class IdentityTests(object):
         # TODO(chungg):add test case once expected behaviour defined
         pass
 
-    def test_role_crud(self):
-        role = {'id': uuid.uuid4().hex, 'name': uuid.uuid4().hex}
-        self.role_api.create_role(role['id'], role)
-        role_ref = self.role_api.get_role(role['id'])
-        role_ref_dict = dict((x, role_ref[x]) for x in role_ref)
-        self.assertDictEqual(role_ref_dict, role)
-
-        role['name'] = uuid.uuid4().hex
-        updated_role_ref = self.role_api.update_role(role['id'], role)
-        role_ref = self.role_api.get_role(role['id'])
-        role_ref_dict = dict((x, role_ref[x]) for x in role_ref)
-        self.assertDictEqual(role_ref_dict, role)
-        self.assertDictEqual(role_ref_dict, updated_role_ref)
-
-        self.role_api.delete_role(role['id'])
-        self.assertRaises(exception.RoleNotFound,
-                          self.role_api.get_role,
-                          role['id'])
-
-    def test_update_role_404(self):
-        role = {'id': uuid.uuid4().hex, 'name': uuid.uuid4().hex}
-        self.assertRaises(exception.RoleNotFound,
-                          self.role_api.update_role,
-                          role['id'],
-                          role)
-
     def test_add_user_to_project(self):
         self.assignment_api.add_user_to_project(self.tenant_baz['id'],
                                                 self.user_foo['id'])
@@ -2216,13 +2153,6 @@ class IdentityTests(object):
 
         parents = self.resource_api.list_project_parents(project1['id'])
         self.assertEqual(0, len(parents))
-
-    def test_list_roles(self):
-        roles = self.role_api.list_roles()
-        self.assertEqual(len(default_fixtures.ROLES), len(roles))
-        role_ids = set(role['id'] for role in roles)
-        expected_role_ids = set(role['id'] for role in default_fixtures.ROLES)
-        self.assertEqual(expected_role_ids, role_ids)
 
     def test_delete_project_with_role_assignments(self):
         tenant = {'id': uuid.uuid4().hex, 'name': uuid.uuid4().hex,
@@ -3266,48 +3196,6 @@ class IdentityTests(object):
         self.assertRaises(exception.ProjectNotFound,
                           self.resource_api.get_project,
                           project_id)
-
-    @tests.skip_if_cache_disabled('role')
-    def test_cache_layer_role_crud(self):
-        role = {'id': uuid.uuid4().hex, 'name': uuid.uuid4().hex}
-        role_id = role['id']
-        # Create role
-        self.role_api.create_role(role_id, role)
-        role_ref = self.role_api.get_role(role_id)
-        updated_role_ref = copy.deepcopy(role_ref)
-        updated_role_ref['name'] = uuid.uuid4().hex
-        # Update role, bypassing the role api manager
-        self.role_api.driver.update_role(role_id, updated_role_ref)
-        # Verify get_role still returns old ref
-        self.assertDictEqual(role_ref, self.role_api.get_role(role_id))
-        # Invalidate Cache
-        self.role_api.get_role.invalidate(self.role_api, role_id)
-        # Verify get_role returns the new role_ref
-        self.assertDictEqual(updated_role_ref,
-                             self.role_api.get_role(role_id))
-        # Update role back to original via the assignment api manager
-        self.role_api.update_role(role_id, role_ref)
-        # Verify get_role returns the original role ref
-        self.assertDictEqual(role_ref, self.role_api.get_role(role_id))
-        # Delete role bypassing the role api manager
-        self.role_api.driver.delete_role(role_id)
-        # Verify get_role still returns the role_ref
-        self.assertDictEqual(role_ref, self.role_api.get_role(role_id))
-        # Invalidate cache
-        self.role_api.get_role.invalidate(self.role_api, role_id)
-        # Verify RoleNotFound is now raised
-        self.assertRaises(exception.RoleNotFound,
-                          self.role_api.get_role,
-                          role_id)
-        # recreate role
-        self.role_api.create_role(role_id, role)
-        self.role_api.get_role(role_id)
-        # delete role via the assignment api manager
-        self.role_api.delete_role(role_id)
-        # verity RoleNotFound is now raised
-        self.assertRaises(exception.RoleNotFound,
-                          self.role_api.get_role,
-                          role_id)
 
     def create_user_dict(self, **attributes):
         user_dict = {'name': uuid.uuid4().hex,
