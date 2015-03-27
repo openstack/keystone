3d805504fa572430155b964546f5216fe3aec006
wanghong <w.wanghong@huawei.com>
2015-03-27 10:51:01 +0800

    add test of /v3/auth/catalog for endpoint_filter
    Change-Id: Ie4f6529a4a798ca03d9bbfead51cee8b20376fb9

--- keystone/tests/unit/test_associate_project_endpoint_extension.py
+++ keystone/tests/unit/test_associate_project_endpoint_extension.py
@@ -576,6 +576,31 @@ class EndpointFilterTokenRequestTestCase(TestExtensionCase):
             endpoint_filter=True,
             ep_filter_assoc=2)
 
+    def test_get_auth_catalog_using_endpoint_filter(self):
+        # add one endpoint to default project
+        self.put('/OS-EP-FILTER/projects/%(project_id)s'
+                 '/endpoints/%(endpoint_id)s' % {
+                     'project_id': self.project['id'],
+                     'endpoint_id': self.endpoint_id},
+                 body='',
+                 expected_status=204)
+
+        auth_data = self.build_authentication_request(
+            user_id=self.user['id'],
+            password=self.user['password'],
+            project_id=self.project['id'])
+        token_data = self.post('/auth/tokens', body=auth_data)
+        self.assertValidProjectScopedTokenResponse(
+            token_data,
+            require_catalog=True,
+            endpoint_filter=True,
+            ep_filter_assoc=1)
+
+        auth_catalog = self.get('/auth/catalog',
+                                token=token_data.headers['X-Subject-Token'])
+        self.assertEqual(token_data.result['token']['catalog'],
+                         auth_catalog.result['catalog'])
+
 
 class JsonHomeTests(TestExtensionCase, test_v3.JsonHomeTestMixin):
     JSON_HOME_DATA = {
