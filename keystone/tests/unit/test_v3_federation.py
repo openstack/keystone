02f8576ddf2fca67849171aa7a1b41ebcfa90ded
Marek Denis <marek.denis@cern.ch>
2015-08-20 18:12:39 +0200

    Ensure ephemeral user's user_id is url-safe
    As a principle, an attribute ``user_id`` should always be url safe and
    we should make sure this is the case especially for the federated
    authentication workflow as user name and id can be provided directly
    from the user (either hardcoded in the mapping rule or passed as a
    parameter from the assertion).
    
    Change-Id: I7256a5e4d31d3c2e55fe956fb0170452bb241078
    Closes-Bug: #1487115

--- keystone/tests/unit/test_v3_federation.py
+++ keystone/tests/unit/test_v3_federation.py
@@ -334,13 +334,17 @@ class FederatedSetupMixin(object):
                         },
                         {
                             'user': {
-                                'name': '{0}'
+                                'name': '{0}',
+                                'id': '{1}'
                             }
                         }
                     ],
                     'remote': [
                         {
                             'type': 'UserName'
+                        },
+                        {
+                            'type': 'Email',
                         },
                         {
                             'type': 'orgPersonType',
@@ -359,13 +363,17 @@ class FederatedSetupMixin(object):
                         },
                         {
                             'user': {
-                                'name': '{0}'
+                                'name': '{0}',
+                                'id': '{1}'
                             }
                         }
                     ],
                     'remote': [
                         {
                             'type': self.ASSERTION_PREFIX + 'UserName'
+                        },
+                        {
+                            'type': self.ASSERTION_PREFIX + 'Email',
                         },
                         {
                             'type': self.ASSERTION_PREFIX + 'orgPersonType',
@@ -384,13 +392,17 @@ class FederatedSetupMixin(object):
                         },
                         {
                             'user': {
-                                'name': '{0}'
+                                'name': '{0}',
+                                'id': '{1}'
                             }
                         }
                     ],
                     'remote': [
                         {
                             'type': 'UserName'
+                        },
+                        {
+                            'type': 'Email'
                         },
                         {
                             'type': 'orgPersonType',
@@ -420,13 +432,17 @@ class FederatedSetupMixin(object):
 
                         {
                             'user': {
-                                'name': '{0}'
+                                'name': '{0}',
+                                'id': '{1}'
                             }
                         }
                     ],
                     'remote': [
                         {
                             'type': 'UserName'
+                        },
+                        {
+                            'type': 'Email'
                         },
                         {
                             'type': 'orgPersonType',
@@ -451,13 +467,17 @@ class FederatedSetupMixin(object):
                         },
                         {
                             'user': {
-                                'name': '{0}'
+                                'name': '{0}',
+                                'id': '{1}'
                             }
                         }
                     ],
                     'remote': [
                         {
                             'type': 'UserName',
+                        },
+                        {
+                            'type': 'Email',
                         },
                         {
                             'type': 'FirstName',
@@ -482,13 +502,17 @@ class FederatedSetupMixin(object):
                         },
                         {
                             'user': {
-                                'name': '{0}'
+                                'name': '{0}',
+                                'id': '{1}'
                             }
                         }
                     ],
                     'remote': [
                         {
                             'type': 'UserName',
+                        },
+                        {
+                            'type': 'Email',
                         },
                         {
                             'type': 'Email',
@@ -509,7 +533,8 @@ class FederatedSetupMixin(object):
                     "local": [
                         {
                             'user': {
-                                'name': '{0}'
+                                'name': '{0}',
+                                'id': '{1}'
                             }
                         },
                         {
@@ -526,6 +551,9 @@ class FederatedSetupMixin(object):
                             'type': 'UserName',
                         },
                         {
+                            'type': 'Email',
+                        },
+                        {
                             "type": "orgPersonType",
                             "any_one_of": [
                                 "CEO",
@@ -538,7 +566,8 @@ class FederatedSetupMixin(object):
                     "local": [
                         {
                             'user': {
-                                'name': '{0}'
+                                'name': '{0}',
+                                'id': '{1}'
                             }
                         },
                         {
@@ -555,6 +584,9 @@ class FederatedSetupMixin(object):
                             "type": "UserName",
                         },
                         {
+                            "type": "Email",
+                        },
+                        {
                             "type": "orgPersonType",
                             "any_one_of": [
                                 "Managers"
@@ -566,7 +598,8 @@ class FederatedSetupMixin(object):
                     "local": [
                         {
                             "user": {
-                                "name": "{0}"
+                                "name": "{0}",
+                                "id": "{1}"
                             }
                         },
                         {
@@ -581,6 +614,9 @@ class FederatedSetupMixin(object):
                     "remote": [
                         {
                             "type": "UserName",
+                        },
+                        {
+                            "type": "Email",
                         },
                         {
                             "type": "UserName",
@@ -1943,7 +1979,8 @@ class MappingRuleEngineTests(FederationTests):
         self.assertValidMappedUserObject(mapped_properties)
         mapped.setup_username({}, mapped_properties)
         self.assertEqual('tbo', mapped_properties['user']['name'])
-        self.assertEqual('tbo', mapped_properties['user']['id'])
+        self.assertEqual('abc123%40example.com',
+                         mapped_properties['user']['id'])
 
     def test_user_identification_id(self):
         """Test varius mapping options and how users are identified.
@@ -1981,17 +2018,28 @@ class MappingRuleEngineTests(FederationTests):
         - Check if user's id is properly set and and equal to value hardcoded
         in the mapping
 
+        This test does two iterations with different assertions used as input
+        for the Mapping Engine.  Different assertions will be matched with
+        different rules in the ruleset, effectively issuing different user_id
+        (hardcoded values). In the first iteration, the hardcoded user_id is
+        not url-safe and we expect Keystone to make it url safe. In the latter
+        iteration, provided user_id is already url-safe and we expect server
+        not to change it.
+
         """
-        mapping = mapping_fixtures.MAPPING_USER_IDS
-        rp = mapping_utils.RuleProcessor(mapping['rules'])
-        assertion = mapping_fixtures.CUSTOMER_ASSERTION
-        mapped_properties = rp.process(assertion)
-        context = {'environment': {}}
-        self.assertIsNotNone(mapped_properties)
-        self.assertValidMappedUserObject(mapped_properties)
-        mapped.setup_username(context, mapped_properties)
-        self.assertEqual('bwilliams', mapped_properties['user']['name'])
-        self.assertEqual('abc123', mapped_properties['user']['id'])
+        testcases = [(mapping_fixtures.CUSTOMER_ASSERTION, 'bwilliams'),
+                     (mapping_fixtures.EMPLOYEE_ASSERTION, 'tbo')]
+        for assertion, exp_user_name in testcases:
+            mapping = mapping_fixtures.MAPPING_USER_IDS
+            rp = mapping_utils.RuleProcessor(mapping['rules'])
+            mapped_properties = rp.process(assertion)
+            context = {'environment': {}}
+            self.assertIsNotNone(mapped_properties)
+            self.assertValidMappedUserObject(mapped_properties)
+            mapped.setup_username(context, mapped_properties)
+            self.assertEqual(exp_user_name, mapped_properties['user']['name'])
+            self.assertEqual('abc123%40example.com',
+                             mapped_properties['user']['id'])
 
 
 class FederatedTokenTests(FederationTests, FederatedSetupMixin):
@@ -2925,12 +2973,12 @@ class FernetFederatedTokenTests(FederationTests, FederatedSetupMixin):
 
     def test_federated_unscoped_token(self):
         resp = self._issue_unscoped_token()
-        self.assertEqual(186, len(resp.headers['X-Subject-Token']))
+        self.assertEqual(204, len(resp.headers['X-Subject-Token']))
 
     def test_federated_unscoped_token_with_multiple_groups(self):
         assertion = 'ANOTHER_CUSTOMER_ASSERTION'
         resp = self._issue_unscoped_token(assertion=assertion)
-        self.assertEqual(204, len(resp.headers['X-Subject-Token']))
+        self.assertEqual(232, len(resp.headers['X-Subject-Token']))
 
     def test_validate_federated_unscoped_token(self):
         resp = self._issue_unscoped_token()
