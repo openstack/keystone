7e2572fbed87441e813379110fa2aa96236875d8
henriquetruta <henrique@lsd.ufcg.edu.br>
2015-09-30 17:46:56 -0300

    Includes server_default option in is_domain column
    is_domain field in project table does not have the server_default
    parameter set. We set it as '0' to comply with migration's default.
    
    Co-Authored-By: Henrique Truta <henrique@lsd.ufcg.edu.br>
    
    Change-Id: Ida6a43ca9b61771f4366e900a46f62cda87f8f27

--- keystone/resource/backends/sql.py
+++ keystone/resource/backends/sql.py
@@ -254,7 +254,8 @@ class Project(sql.ModelBase, sql.DictBase):
     enabled = sql.Column(sql.Boolean)
     extra = sql.Column(sql.JsonBlob())
     parent_id = sql.Column(sql.String(64), sql.ForeignKey('project.id'))
-    is_domain = sql.Column(sql.Boolean, default=False, nullable=False)
+    is_domain = sql.Column(sql.Boolean, default=False, nullable=False,
+                           server_default='0')
     # Unique constraint across two columns to create the separation
     # rather than just only 'name' being unique
     __table_args__ = (sql.UniqueConstraint('domain_id', 'name'), {})
