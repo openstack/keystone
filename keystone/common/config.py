10cf8cfd427b0703cb32e6dddc290e33b7119224
Steve Martinelli <stevemar@ca.ibm.com>
2015-11-19 00:43:00 -0500

    deprecate `enabled` option for endpoint-policy extension
    the endpoint-policy extension, like the other recently moved
    extensions, should be always enabled. Since this option made
    its way into the liberty release we need to mark the option
    as deprecated in M and remove it in O
    
    implements bp: move-extensions
    
    Change-Id: I5c01da0663c636bc6e2d9f12ac02b53edb9a277f

--- keystone/common/config.py
+++ keystone/common/config.py
@@ -486,6 +486,7 @@ FILE_OPTIONS = {
     'endpoint_policy': [
         cfg.BoolOpt('enabled',
                     default=True,
+                    deprecated_for_removal=True,
                     help='Enable endpoint_policy functionality.'),
         cfg.StrOpt('driver',
                    default='sql',
