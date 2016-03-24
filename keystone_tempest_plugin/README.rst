088393d5216b49f909abcb1758eb4d3ebd88c980
Rodrigo Duarte <rduartes@redhat.com>
2016-03-24 15:08:44 -0300

    Base for keystone tempest plugin
    This patch adds the basic files and configs in order to enable the
    keystone tempest plugin interface using tempest-plugin-cookiecutter.
    Since we are adding them inside keystone's repository, they can be
    installed alongside keystone and to run the tests (when we have one)
    we simply use `testr run keystone_tempest_plugin` in tempest.
    
    For more details about the tempest plugin interface see [1]
    
    [1] http://docs.openstack.org/developer/tempest/plugin.html
    
    Change-Id: Ia42e79246251e8af1010aa8eaf462aacf75644a7
    Partially-Implements: bp keystone-tempest-plugin-tests

---
+++ keystone_tempest_plugin/README.rst
@@ -0,0 +1,6 @@
+==========================
+Tempest tests for keystone
+==========================
+
+This directory contains Tempest tests to cover the keystone project.
+
---
+++ keystone_tempest_plugin/plugin.py
@@ -0,0 +1,39 @@
+# Copyright 2016 Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import os
+
+from tempest import config
+from tempest.test_discover import plugins
+
+from keystone_tempest_plugin import config as project_config
+
+
+class KeystoneTempestPlugin(plugins.TempestPlugin):
+    def load_tests(self):
+        base_path = os.path.split(os.path.dirname(
+            os.path.abspath(__file__)))[0]
+        test_dir = "keystone_tempest_plugin/tests"
+        full_test_dir = os.path.join(base_path, test_dir)
+        return full_test_dir, base_path
+
+    def register_opts(self, conf):
+        config.register_opt_group(conf, project_config.identity_group,
+                                  project_config.IdentityGroup)
+
+    def get_opt_lists(self):
+        return [(project_config.identity_group.name,
+                 project_config.IdentityGroup)]
