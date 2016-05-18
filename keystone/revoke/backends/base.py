47529d0e3219028c8c5a0dd50382cdb395c1b467
Ronald De Rose <ronald.de.rose@intel.com>
2016-05-18 00:17:52 +0000

    Move the revoke abstract base class out of core
    This patch moves the revoke abstract base class out of core and into
    backends/base.py
    
    This removes dependencies where backend code references code in the
    core. The reasoning being that the core should know about the backend
    interface, but the backends should not know anything about the core
    (separation of concerns). And part of the risk here is a potential for
    circular dependencies.
    
    Partial-Bug: #1563101
    
    Change-Id: I0550e0fedfb28b16e9803d225df4d2337e6c65e6

---
+++ keystone/revoke/backends/base.py
@@ -0,0 +1,60 @@
+# Copyright 2012 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import abc
+import datetime
+
+from oslo_config import cfg
+from oslo_utils import timeutils
+import six
+
+from keystone import exception
+
+
+CONF = cfg.CONF
+
+
+def revoked_before_cutoff_time():
+    expire_delta = datetime.timedelta(
+        seconds=CONF.token.expiration + CONF.revoke.expiration_buffer)
+    oldest = timeutils.utcnow() - expire_delta
+    return oldest
+
+
+@six.add_metaclass(abc.ABCMeta)
+class RevokeDriverV8(object):
+    """Interface for recording and reporting revocation events."""
+
+    @abc.abstractmethod
+    def list_events(self, last_fetch=None):
+        """return the revocation events, as a list of objects.
+
+        :param last_fetch:   Time of last fetch.  Return all events newer.
+        :returns: A list of keystone.revoke.model.RevokeEvent
+                  newer than `last_fetch.`
+                  If no last_fetch is specified, returns all events
+                  for tokens issued after the expiration cutoff.
+
+        """
+        raise exception.NotImplemented()  # pragma: no cover
+
+    @abc.abstractmethod
+    def revoke(self, event):
+        """register a revocation event.
+
+        :param event: An instance of
+            keystone.revoke.model.RevocationEvent
+
+        """
+        raise exception.NotImplemented()  # pragma: no cover
