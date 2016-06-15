8c2412adecc08e7bcb07a3c679fcb45fdc06aeb7
Liam Young <liam.young@canonical.com>
2016-06-15 10:01:43 +0000

    Correct domain_id and name constraint dropping
    The 'domain_id' and 'name' unique constraint was not properly dropped
    in some cases because the unique constraint was not consistently
    named. In all cases we must search for the constraint expected,
    not assume the name of the constraint will be consistent
    (especially from older installs that have been moved forward in
    releases). This fix is modeled on the fix for a similair issue
    authored by Morgan Fainberg & Matthew Thode for Bug #1562934
    
    Migration 091:
        Fix to broken migration to prevent failed migrations when database is
        upgraded from Kilo (or below) to Mitaka
    Migration 097:
        Ensure that when Mitaka point release is applied the constraint and tables
        have been dropped if migration 91 was previously worked around.
    Migration 104:
        Ensure that when upgrading to Newton the constraint and tables
        have been dropped if migration 91 was previously worked around.
    
    Migration 91 drops 3 columns from the user table after the code to disable
    the constraint. I have included code in migrations 97 and 104 to also drop
    those columns if they are still present in case they were missed when working
    around Bug #1572341. This may be over kill.
    
    Change-Id: I076d7139b388e30be8826d0a4550256b5617d992
    Closes-bug: #1572341

---
+++ keystone/common/sql/migrate_repo/versions/097_drop_user_name_domainid_constraint.py
@@ -0,0 +1,67 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import migrate
+import sqlalchemy as sql
+
+_USER_TABLE_NAME = 'user'
+_USER_NAME_COLUMN_NAME = 'name'
+_USER_DOMAINID_COLUMN_NAME = 'domain_id'
+_USER_PASSWORD_COLUMN_NAME = 'password'
+
+
+def upgrade(migrate_engine):
+    meta = sql.MetaData()
+    meta.bind = migrate_engine
+
+    user_table = sql.Table(_USER_TABLE_NAME, meta, autoload=True)
+
+    # NOTE(gnuoy): the `domain_id` unique constraint is not guaranteed to
+    # be a fixed name, such as 'ixu_user_name_domain_id`, so we need to
+    # search for the correct constraint that only affects
+    # user_table.c.domain_id and drop that constraint. (Fix based on
+    # morganfainbergs fix in 088_domain_specific_roles.py)
+    #
+    # This is an idempotent change that reflects the fix to migration
+    # 91 if the user name & domain_id unique constraint was not named
+    # consistently and someone manually fixed the migrations / db
+    # without dropping the old constraint.
+    to_drop = None
+    if migrate_engine.name == 'mysql':
+        for index in user_table.indexes:
+            if (index.unique and len(index.columns) == 2 and
+                    _USER_DOMAINID_COLUMN_NAME in index.columns and
+                    _USER_NAME_COLUMN_NAME in index.columns):
+                to_drop = index
+                break
+    else:
+        for index in user_table.constraints:
+            if (len(index.columns) == 2 and
+                    _USER_DOMAINID_COLUMN_NAME in index.columns and
+                    _USER_NAME_COLUMN_NAME in index.columns):
+                to_drop = index
+                break
+
+    # remove domain_id and name unique constraint
+    if to_drop is not None:
+        migrate.UniqueConstraint(user_table.c.domain_id,
+                                 user_table.c.name,
+                                 name=to_drop.name).drop()
+
+    # If migration 91 was aborted due to Bug #1572341 then columns may not
+    # have been dropped.
+    if _USER_DOMAINID_COLUMN_NAME in user_table.c:
+        user_table.c.domain_id.drop()
+    if _USER_NAME_COLUMN_NAME in user_table.c:
+        user_table.c.name.drop()
+    if _USER_PASSWORD_COLUMN_NAME in user_table.c:
+        user_table.c.password.drop()
