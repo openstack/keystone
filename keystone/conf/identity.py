a6c62714b0fde62e987363bd2dfc63a09a81258a
Dolph Mathews <dolph.mathews@gmail.com>
2016-06-29 16:20:46 -0500

    Improve keystone.conf [identity] documentation
    Change-Id: I890e290e6a651135ab53e7d2e780ca960683577b

--- keystone/conf/identity.py
+++ keystone/conf/identity.py
@@ -21,11 +21,12 @@ default_domain_id = cfg.StrOpt(
     default='default',
     help=utils.fmt("""
 This references the domain to use for all Identity API v2 requests (which are
-not aware of domains). A domain with this ID will be created for you by
-keystone-manage db_sync in migration 008. The domain referenced by this ID
-cannot be deleted on the v3 API, to prevent accidentally breaking the v2 API.
-There is nothing special about this domain, other than the fact that it must
-exist to order to maintain support for your v2 clients.
+not aware of domains). A domain with this ID will be optionally created for you
+by `keystone-manage bootstrap`. The domain referenced by this ID cannot be
+deleted on the v3 API, to prevent accidentally breaking the v2 API. There is
+nothing special about this domain, other than the fact that it must exist to
+order to maintain support for your v2 clients. There is typically no reason to
+change this value.
 """))
 
 domain_specific_drivers_enabled = cfg.BoolOpt(
@@ -35,35 +36,45 @@ domain_specific_drivers_enabled = cfg.BoolOpt(
 A subset (or all) of domains can have their own identity driver, each with
 their own partial configuration options, stored in either the resource backend
 or in a file in a domain configuration directory (depending on the setting of
-domain_configurations_from_database). Only values specific to the domain need
-to be specified in this manner. This feature is disabled by default; set to
-true to enable.
+`[identity] domain_configurations_from_database`). Only values specific to the
+domain need to be specified in this manner. This feature is disabled by
+default, but may be enabled by default in a future release; set to true to
+enable.
 """))
 
 domain_configurations_from_database = cfg.BoolOpt(
     'domain_configurations_from_database',
     default=False,
     help=utils.fmt("""
-Extract the domain specific configuration options from the resource backend
-where they have been stored with the domain data. This feature is disabled by
-default (in which case the domain specific options will be loaded from files in
-the domain configuration directory); set to true to enable.
+By default, domain-specific configuration data is read from files in the
+directory identified by `[identity] domain_config_dir`. Enabling this
+configuration option allows you to instead manage domain-specific
+configurations through the API, which are then persisted in the backend
+(typically, a SQL database), rather than using configuration files on disk.
 """))
 
 domain_config_dir = cfg.StrOpt(
     'domain_config_dir',
     default='/etc/keystone/domains',
     help=utils.fmt("""
-Path for Keystone to locate the domain specific identity configuration files if
-domain_specific_drivers_enabled is set to true.
+Absolute path where keystone should locate domain-specific `[identity]`
+configuration files. This option has no effect unless `[identity]
+domain_specific_drivers_enabled` is set to true. There is typically no reason
+to change this value.
 """))
 
 driver = cfg.StrOpt(
     'driver',
     default='sql',
     help=utils.fmt("""
-Entrypoint for the identity backend driver in the keystone.identity namespace.
-Supplied drivers are ldap and sql.
+Entry point for the identity backend driver in the `keystone.identity`
+namespace. Keystone provides a `sql` and `ldap` driver. This option is also
+used as the default driver selection (along with the other configuration
+variables in this section) in the event that `[identity]
+domain_specific_drivers_enabled` is enabled, but no applicable domain-specific
+configuration is defined for the domain in question. Unless your deployment
+primarily relies on `ldap` AND is not using domain-specific configuration, you
+should typically leave this set to `sql`.
 """))
 
 caching = cfg.BoolOpt(
@@ -71,7 +82,7 @@ caching = cfg.BoolOpt(
     default=True,
     help=utils.fmt("""
 Toggle for identity caching. This has no effect unless global caching is
-enabled.
+enabled. There is typically no reason to disable this.
 """))
 
 cache_time = cfg.IntOpt(
@@ -87,8 +98,8 @@ max_password_length = cfg.IntOpt(
     default=4096,
     max=passlib.utils.MAX_PASSWORD_SIZE,
     help=utils.fmt("""
-Maximum supported length for user passwords; decrease to improve
-performance.
+Maximum allowed length for user passwords. Decrease this value to improve
+performance. Changing this value does not effect existing passwords.
 """))
 
 list_limit = cfg.IntOpt(
