a59aa8b5c6c9b22cc88754c6ecc3123e9ba18c7f
Eric Brown <browne@vmware.com>
2016-06-30 00:50:41 -0700

    Use min to avoid checking < 1 max fernet keys
    If the IntOpt that represents max_active_keys for fernet tokens
    has a minimum set to 1, then there is no need to have additional
    code to check for this case. It also helps deployers avoid
    misconfiguration.
    
    Change-Id: I767cc6bb0e6de93c570ee61f0ea5ef83953f5044

--- keystone/token/providers/fernet/utils.py
+++ keystone/token/providers/fernet/utils.py
@@ -209,12 +209,6 @@ def rotate_keys(keystone_user_id=None, keystone_group_id=None):
     _create_new_key(keystone_user_id, keystone_group_id)
 
     max_active_keys = CONF.fernet_tokens.max_active_keys
-    # check for bad configuration
-    if max_active_keys < 1:
-        LOG.warning(_LW(
-            '[fernet_tokens] max_active_keys must be at least 1 to maintain a '
-            'primary key.'))
-        max_active_keys = 1
 
     # purge excess keys
 
