294c5a11ca6dc61d2cd873e6460a1b39a256a9c4
Lance Bragstad <lbragstad@gmail.com>
2016-09-14 15:37:32 +0000

    Remove unused read_cached_file method from utils
    The read_cached_file() method in keystone.common.utils isn't being
    used anywhere in keystone and it isn't tested. Let's remove it.
    
    Change-Id: Iafba37114957c5270351aafd25538c6085c07805

--- keystone/common/utils.py
+++ keystone/common/utils.py
@@ -86,26 +86,6 @@ def flatten_dict(d, parent_key=''):
     return dict(items)
 
 
-def read_cached_file(filename, cache_info, reload_func=None):
-    """Read from a file if it has been modified.
-
-    :param cache_info: dictionary to hold opaque cache.
-    :param reload_func: optional function to be called with data when
-                        file is reloaded due to a modification.
-
-    :returns: data from file.
-
-    """
-    mtime = os.path.getmtime(filename)
-    if not cache_info or mtime != cache_info.get('mtime'):
-        with open(filename) as fap:
-            cache_info['data'] = fap.read()
-        cache_info['mtime'] = mtime
-        if reload_func:
-            reload_func(cache_info['data'])
-    return cache_info['data']
-
-
 class SmarterEncoder(jsonutils.json.JSONEncoder):
     """Help for JSON encoding dict-like objects."""
 
