0818d42cb2afb5cd1ded72d9c2cc7c0ac4762a2c
David Stanek <dstanek@dstanek.com>
2016-09-15 19:28:22 +0000

    Ensure the sqla-migrate scripts cache is cleared
    The cache was causing problems with our multiple repositories since
    the modules all have the same names.
      e.g. expand_repo/versions/001_awesome.py and
           data_migration_repo/versions/001_awesome.py)
    
    Change-Id: Ib9bbeebc2149b0aee5f3d6fec963e40e90f1b743
    Closes-Bug: #1620764

--- keystone/tests/unit/test_sql_upgrade.py
+++ keystone/tests/unit/test_sql_upgrade.py
@@ -37,6 +37,7 @@ import uuid
 import migrate
 from migrate.versioning import api as versioning_api
 from migrate.versioning import repository
+from migrate.versioning import script
 import mock
 from oslo_db import exception as db_exception
 from oslo_db.sqlalchemy import migration
@@ -226,6 +227,11 @@ class MigrationRepository(object):
 class SqlMigrateBase(test_base.DbTestCase):
     def setUp(self):
         super(SqlMigrateBase, self).setUp()
+
+        # NOTE(dstanek): Clear out sqlalchemy-migrate's script cache to allow
+        # us to have multiple repos (expand, migrate, contrate) where the
+        # modules have the same name (001_awesome.py).
+        self.addCleanup(script.PythonScript.clear)
 
         # Set keystone's connection URL to be the test engine's url.
         database.initialize_sql_session(self.engine.url)
