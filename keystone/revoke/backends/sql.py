477189d0c51a3fd2f8709827fb635934f0e74200
Ronald De Rose <ronald.de.rose@intel.com>
2016-09-26 13:59:46 +0000

    Add revocation event indexes
    This patch adds indexes to the revocation_event table as part of an
    effort to improve performance during token validation.
    
    Partial-Bug: 1524030
    Change-Id: I73ff077bb6dc3ca8821f8cc14639bf986517d158

--- keystone/revoke/backends/sql.py
+++ keystone/revoke/backends/sql.py
@@ -31,11 +31,19 @@ class RevocationEvent(sql.ModelBase, sql.ModelDictMixin):
     trust_id = sql.Column(sql.String(64))
     consumer_id = sql.Column(sql.String(64))
     access_token_id = sql.Column(sql.String(64))
-    issued_before = sql.Column(sql.DateTime(), nullable=False)
+    issued_before = sql.Column(sql.DateTime(), nullable=False, index=True)
     expires_at = sql.Column(sql.DateTime())
     revoked_at = sql.Column(sql.DateTime(), nullable=False, index=True)
     audit_id = sql.Column(sql.String(32))
     audit_chain_id = sql.Column(sql.String(32))
+    __table_args__ = (
+        sql.Index('ix_revocation_event_project_id_issued_before', 'project_id',
+                  'issued_before'),
+        sql.Index('ix_revocation_event_user_id_issued_before', 'user_id',
+                  'issued_before'),
+        sql.Index('ix_revocation_event_audit_id_issued_before',
+                  'audit_id', 'issued_before'),
+    )
 
 
 class Revoke(base.RevokeDriverBase):
