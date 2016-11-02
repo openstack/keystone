2d5641516120027bac42a923c2611031ab63a958
Richard Avelar <csravelar@gmail.com>
2016-11-02 16:49:01 +0000

    Fix test_revoke to run all tests after pki removal
    The recent change [1] caused the removal of a certain class that
    looked specific to PKI and PKIz testing and implementation, however,
    actually was being used to test all supported token providers. This
    caused a chunk of tests to simply not run at all and therefore be
    overlooked. This change adds support for both UUID and Fernet so that
    all tests are being run once again and for all available token
    providers.
    
    [1]: Icf1ebced44a675c88fb66a6c0431208ff5181574
    Change-Id: I5365e2ed74ea55377729d0910cc2892d2bb8889c

--- keystone/tests/unit/test_revoke.py
+++ keystone/tests/unit/test_revoke.py
@@ -19,11 +19,17 @@ from oslo_utils import timeutils
 from six.moves import range
 
 from keystone.common import utils
+import keystone.conf
 from keystone import exception
 from keystone.models import revoke_model
 from keystone.revoke.backends import sql
 from keystone.tests import unit
+from keystone.tests.unit import ksfixtures
+from keystone.tests.unit import test_backend_sql
 from keystone.token.providers import common
+
+
+CONF = keystone.conf.CONF
 
 
 def _future_time():
@@ -364,6 +370,31 @@ class RevokeTests(object):
         self.assertRaises(exception.TokenNotFound,
                           self.revoke_api.check_token,
                           token_values)
+
+
+class UUIDSqlRevokeTests(test_backend_sql.SqlTests, RevokeTests):
+    def config_overrides(self):
+        super(UUIDSqlRevokeTests, self).config_overrides()
+        self.config_fixture.config(
+            group='token',
+            provider='uuid',
+            revoke_by_id=False)
+
+
+class FernetSqlRevokeTests(test_backend_sql.SqlTests, RevokeTests):
+    def config_overrides(self):
+        super(FernetSqlRevokeTests, self).config_overrides()
+        self.config_fixture.config(
+            group='token',
+            provider='fernet',
+            revoke_by_id=False)
+        self.useFixture(
+            ksfixtures.KeyRepository(
+                self.config_fixture,
+                'fernet_tokens',
+                CONF.fernet_tokens.max_active_keys
+            )
+        )
 
 
 def add_event(events, event):
