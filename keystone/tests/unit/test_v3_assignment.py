aa531a001a31b757fa12412bf0eb84e4c8d67bb0
Samuel de Medeiros Queiroz <samueldmq@gmail.com>
2016-12-06 11:57:41 -0300

    Add test to expose bug 1625230
    When listing role assignments with ?include_names, the inheritance flag
    was not returned properly.
    
    Related-Bug: #1625230
    
    Change-Id: Ibe467246da90d30cb40564667642056da4816139

--- keystone/tests/unit/test_v3_assignment.py
+++ keystone/tests/unit/test_v3_assignment.py
@@ -23,6 +23,7 @@ import keystone.conf
 from keystone import exception
 from keystone.tests import unit
 from keystone.tests.unit import test_v3
+from keystone.tests.unit import utils as test_utils
 
 
 CONF = keystone.conf.CONF
@@ -1707,6 +1708,58 @@ class AssignmentInheritanceTestCase(test_v3.RestfulTestCase,
         self.role_api.create_role(role['id'], role)
 
         self._test_list_role_assignments_include_names(role)
+
+    @test_utils.wip('Blocked by bug #1625230')
+    def test_list_inherited_role_assignments_include_names(self):
+        """Call ``GET /role_assignments?include_names``.
+
+        Test goal: ensure calling list role assignments including names
+        honors the inherited role assignments flag.
+
+        Test plan:
+        - Create a role and a domain with a user;
+        - Create a inherited role assignment;
+        - List role assignments for that user;
+        - List role assignments for that user including names.
+
+        """
+        role = unit.new_role_ref()
+        self.role_api.create_role(role['id'], role)
+        domain = unit.new_domain_ref()
+        self.resource_api.create_domain(domain['id'], domain)
+        user = unit.create_user(self.identity_api, domain_id=domain['id'])
+
+        # Create and store expected assignment refs
+        assignment = self.build_role_assignment_entity(
+            role_id=role['id'], domain_id=domain['id'], user_id=user['id'],
+            inherited_to_projects=True)
+        assignment_names = self.build_role_assignment_entity_include_names(
+            role_ref=role, domain_ref=domain, user_ref=user,
+            inherited_assignment=True)
+
+        # Ensure expected assignment refs are inherited and have the same URL
+        self.assertEqual('projects',
+                         assignment['scope']['OS-INHERIT:inherited_to'])
+        self.assertEqual('projects',
+                         assignment_names['scope']['OS-INHERIT:inherited_to'])
+        self.assertEqual(assignment['links']['assignment'],
+                         assignment_names['links']['assignment'])
+
+        self.put(assignment['links']['assignment'])
+
+        collection_url = '/role_assignments?user.id=%(user_id)s' % (
+                         {'user_id': user['id']})
+        result = self.get(collection_url)
+        self.assertValidRoleAssignmentListResponse(
+            result, expected_length=1, resource_url=collection_url)
+        self.assertRoleAssignmentInListResponse(result, assignment)
+
+        collection_url = ('/role_assignments?include_names&'
+                          'user.id=%(user_id)s' % {'user_id': user['id']})
+        result = self.get(collection_url)
+        self.assertValidRoleAssignmentListResponse(
+            result, expected_length=1, resource_url=collection_url)
+        self.assertRoleAssignmentInListResponse(result, assignment_names)
 
     def test_list_role_assignments_for_disabled_inheritance_extension(self):
         """Call ``GET /role_assignments with inherited domain grants``.
