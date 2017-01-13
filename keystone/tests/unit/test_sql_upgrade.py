066bf83a810636ef0827e6760860526b9cc35c97
Steve Martinelli <s.martinelli@gmail.com>
2017-01-13 00:15:35 -0500

    fix broken links
    as noted in [1] we had a few broken links, this should fix the
    ones seen in the keystone repo
    
    [1] http://lists.openstack.org/pipermail/openstack-docs/2017-January/009464.html
    
    Change-Id: I7d4c6d7646a76a35da651c7c8d6e0bd87a8cefe6

--- keystone/tests/unit/test_sql_upgrade.py
+++ keystone/tests/unit/test_sql_upgrade.py
@@ -1606,7 +1606,7 @@ class VersionTests(SqlMigrateBase):
 
         For more information, see "Database Migrations" here:
 
-            http://docs.openstack.org/developer/keystone/developing.html
+            http://docs.openstack.org/developer/keystone/devref/development_best_practices.html#database-migrations
 
         """
         # Note to reviewers: this version number should never change.
@@ -1629,7 +1629,7 @@ class VersionTests(SqlMigrateBase):
 
         For more information, see "Database Migrations" here:
 
-            http://docs.openstack.org/developer/keystone/developing.html
+            http://docs.openstack.org/developer/keystone/devref/development_best_practices.html#database-migrations
 
         """
         # Transitive comparison: expand == data migration == contract
