6e0faa96eda7901b2e64aaf7d255c52527392b59
Ronald De Rose <ronald.de.rose@intel.com>
2017-01-21 20:47:04 +0000

    Refactor shadow users tests
    The main reason for this change is to create real Identity Provider data
    for the shadow users (backend and core) tests related to federated
    users. This is needed in a subsequent patch where we set the domain for
    federated users.
    
    I ended up moving the shadow user tests out of the identity tests, as my
    changes were conflicting with some of those tests and it provided a
    simpler implementation.
    
    Partial-Bug: #1642687
    Partially-Implements: bp support-federated-attr
    Depends-On: I08a8f3cb59150c8e9a2f90c5ea6b0aa197a03572
    Change-Id: If8c8ad39c4c55a2d800bf4432411db59799e84e6

---
+++ keystone/tests/unit/test_shadow_users.py
@@ -0,0 +1,52 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import uuid
+
+from keystone.tests import unit
+from keystone.tests.unit.identity.shadow_users import test_backend
+from keystone.tests.unit.identity.shadow_users import test_core
+from keystone.tests.unit.ksfixtures import database
+
+
+class ShadowUsersTests(unit.TestCase,
+                       test_backend.ShadowUsersBackendTests,
+                       test_core.ShadowUsersCoreTests):
+    def setUp(self):
+        super(ShadowUsersTests, self).setUp()
+        self.useFixture(database.Database())
+        self.load_backends()
+        self.idp = {
+            'id': uuid.uuid4().hex,
+            'enabled': True,
+            'description': uuid.uuid4().hex
+        }
+        self.mapping = {
+            'id': uuid.uuid4().hex,
+        }
+        self.protocol = {
+            'id': uuid.uuid4().hex,
+            'idp_id': self.idp['id'],
+            'mapping_id': self.mapping['id']
+        }
+        self.federated_user = {
+            'idp_id': self.idp['id'],
+            'protocol_id': self.protocol['id'],
+            'unique_id': uuid.uuid4().hex,
+            'display_name': uuid.uuid4().hex
+        }
+        self.federation_api.create_idp(self.idp['id'], self.idp)
+        self.federation_api.create_mapping(self.mapping['id'], self.mapping)
+        self.federation_api.create_protocol(
+            self.idp['id'], self.protocol['id'], self.protocol)
+        self.domain_id = (
+            self.federation_api.get_idp(self.idp['id'])['domain_id'])
