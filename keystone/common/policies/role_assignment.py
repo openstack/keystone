de7e79fdf3354c6fceb3e74b60f308590ec2f005
Lance Bragstad <lbragstad@gmail.com>
2017-12-06 19:59:49 +0000

    Add scope_types to role assignment policies
    Change-Id: Ia56c7fc32607c2893ba172d528f29a34e32573a1

--- keystone/common/policies/role_assignment.py
+++ keystone/common/policies/role_assignment.py
@@ -18,6 +18,15 @@ role_assignment_policies = [
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'list_role_assignments',
         check_str=base.RULE_ADMIN_REQUIRED,
+        # FIXME(lbragstad): This API will behave differently depending on the
+        # token scope used to call the API. A system administrator should be
+        # able to list all role assignment across the entire deployment. A
+        # project or domain administrator should be able to list role
+        # assignments within the domain or project they administer. Once we
+        # make keystone smart enough to handle those cases in code, we can add
+        # 'project' to the scope_types below. For now, this should be a system
+        # administrator only operation to maintain backwards compatibility.
+        scope_types=['system'],
         description='List role assignments.',
         operations=[{'path': '/v3/role_assignments',
                      'method': 'GET'},
@@ -26,6 +35,14 @@ role_assignment_policies = [
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'list_role_assignments_for_tree',
         check_str=base.RULE_ADMIN_REQUIRED,
+        # NOTE(lbragstad): This is purely a project-scoped operation. The
+        # project tree is calculated based on the project scope of the token
+        # used to make the request. System administrators would have to find a
+        # way to supply a project scope with a system-scoped token, which
+        # defeats the purpose. System administrators can list all role
+        # assignments anyway, so the usefulness of an API that returns a subset
+        # is negligible when they have access to the entire set.
+        scope_types=['project'],
         description=('List all role assignments for a given tree of '
                      'hierarchical projects.'),
         operations=[{'path': '/v3/role_assignments?include_subtree',
