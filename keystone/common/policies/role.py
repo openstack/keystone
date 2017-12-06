7867efbaba68cece8dc365f18c54c6eb3cf6ca98
Lance Bragstad <lbragstad@gmail.com>
2017-12-06 20:06:32 +0000

    Add scope_types to role policies
    This commit sets the scope types for role policies to `system`. When
    keystone supports full role management without having to modify
    service policy files we should consider adding `project` to the
    list of scope types.
    
    From now on, a warning will be logged when a project-scoped token is
    used to manage roles. Operators can opt into requiring system-scoped
    tokens for these policies by enabling oslo.policy's `enable_scope`
    configuration option, which will result in an HTTP Forbidden
    exception when mismatching scope is detected.
    
    Change-Id: I7eb7aef7dbf99a86feacb9346090a02a7bd97e7c

--- keystone/common/policies/role.py
+++ keystone/common/policies/role.py
@@ -18,6 +18,12 @@ role_policies = [
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'get_role',
         check_str=base.RULE_ADMIN_REQUIRED,
+        # FIXME(lbragstad): Roles should be considered a system-level resource.
+        # The current RBAC design of OpenStack requires configuration
+        # modification depending on the roles created in keystone. Once that is
+        # no longer true we should consider adding `project` to the list of
+        # scope_types.
+        scope_types=['system'],
         description='Show role details.',
         operations=[{'path': '/v3/roles/{role_id}',
                      'method': 'GET'},
@@ -26,6 +32,7 @@ role_policies = [
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'list_roles',
         check_str=base.RULE_ADMIN_REQUIRED,
+        scope_types=['system'],
         description='List roles.',
         operations=[{'path': '/v3/roles',
                      'method': 'GET'},
@@ -34,24 +41,33 @@ role_policies = [
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'create_role',
         check_str=base.RULE_ADMIN_REQUIRED,
+        scope_types=['system'],
         description='Create role.',
         operations=[{'path': '/v3/roles',
                      'method': 'POST'}]),
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'update_role',
         check_str=base.RULE_ADMIN_REQUIRED,
+        scope_types=['system'],
         description='Update role.',
         operations=[{'path': '/v3/roles/{role_id}',
                      'method': 'PATCH'}]),
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'delete_role',
         check_str=base.RULE_ADMIN_REQUIRED,
+        scope_types=['system'],
         description='Delete role.',
         operations=[{'path': '/v3/roles/{role_id}',
                      'method': 'DELETE'}]),
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'get_domain_role',
         check_str=base.RULE_ADMIN_REQUIRED,
+        # FIXME(lbragstad): Once OpenStack supports a way to make role changes
+        # without having to modify policy files, scope_types for
+        # domain-specific roles should include `project`. This will expose
+        # these APIs to domain/project administrators, allowing them to create,
+        # modify, and delete roles for their own projects and domains.
+        scope_types=['system'],
         description='Show domain role.',
         operations=[{'path': '/v3/roles/{role_id}',
                      'method': 'GET'},
@@ -61,6 +77,7 @@ role_policies = [
         name=base.IDENTITY % 'list_domain_roles',
         check_str=base.RULE_ADMIN_REQUIRED,
         description='List domain roles.',
+        scope_types=['system'],
         operations=[{'path': '/v3/roles?domain_id={domain_id}',
                      'method': 'GET'},
                     {'path': '/v3/roles?domain_id={domain_id}',
@@ -69,18 +86,21 @@ role_policies = [
         name=base.IDENTITY % 'create_domain_role',
         check_str=base.RULE_ADMIN_REQUIRED,
         description='Create domain role.',
+        scope_types=['system'],
         operations=[{'path': '/v3/roles',
                      'method': 'POST'}]),
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'update_domain_role',
         check_str=base.RULE_ADMIN_REQUIRED,
         description='Update domain role.',
+        scope_types=['system'],
         operations=[{'path': '/v3/roles/{role_id}',
                      'method': 'PATCH'}]),
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'delete_domain_role',
         check_str=base.RULE_ADMIN_REQUIRED,
         description='Delete domain role.',
+        scope_types=['system'],
         operations=[{'path': '/v3/roles/{role_id}',
                      'method': 'DELETE'}])
 ]
