9575937e56295a5695336598a90a4cb1d3b516af
Mike Perez <thingee@gmail.com>
2017-12-14 10:27:34 +1100

    Replace support matrix ext with common library
    The code to generate a support matrix has been pulled into a common
    library. Using this instead of duplicating code in various projects that
    need it.
    
    Change-Id: Ib15c086ceb84029e96be0fc60bd64f2a7a3e41aa
    Co-Authored-By: Stephen Finucane <stephenfin@redhat.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,19 +24,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import os
-import sys
-
-# NOTE(dstanek): adds _ to the builtins so keystone modules can be imported
-__builtins__['_'] = str
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('./'))
-
 # -- General configuration ----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -52,9 +39,9 @@ extensions = ['sphinx.ext.coverage',
               'oslo_policy.sphinxpolicygen',
               'openstackdocstheme',
               'oslo_policy.sphinxext',
-              'ext.support_matrix',
               'sphinxcontrib.apidoc',
               'sphinxcontrib.seqdiag',
+              'sphinx_feature_classification.support_matrix',
               ]
 
 # sphinxcontrib.apidoc options
