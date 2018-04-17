3b701cdf70d7331c781a6b1a46dd8247906b9b63
wangxiyuan <wangxiyuan@huawei.com>
2018-04-17 19:21:43 +0800

    Invalidate the shadow user cache when deleting a user
    When deleting a user, the cache for the related shadow user should
    be invalidated as well. Otherwise the federation authentication
    will not work well and will raise 404 UserNotFound error.
    
    This patch fixes the bug and adds a new function for shadow backend
    to get the shadow user information.
    
    Change-Id: I3882f0dc6e8f8f618bb89ebd699736bc4b352261
    Closes-bug: #1760205

--- keystone/identity/shadow_backends/base.py
+++ keystone/identity/shadow_backends/base.py
@@ -88,3 +88,16 @@ class ShadowUsersDriverBase(object):
 
         """
         raise exception.NotImplemented()
+
+    @abc.abstractmethod
+    def list_federated_users_info(self, hints=None):
+        """Get the shadow users info with the specified filters.
+
+        :param hints: contains the list of filters yet to be satisfied.
+                      Any filters satisfied here will be removed so that
+                      the caller will know if any filters remain.
+        :returns list: A list of objects that containing the shadow users
+                       reference.
+
+        """
+        raise exception.NotImplemented()
