a7d4a9bd4c620dae127b53fed8bdaf0ea3055635
wangxiyuan <wangxiyuan@huawei.com>
2018-07-05 09:40:50 +0800

    Update project depth check
    when a hierarchical limit enforcement model is deployed
    by keystone, the project hierarchical depth should
    not break the limit model as well.
    
    This patch updated the project depth check function
    to fix the problem.
    
    bp: strict-two-level-model
    Change-Id: I695fedcf83bdca2946a2c5d876052c02b0f63810

--- keystone/tests/unit/test_v3_resource.py
+++ keystone/tests/unit/test_v3_resource.py
@@ -1798,3 +1798,32 @@ class ResourceTestCase(test_v3.RestfulTestCase,
         resp = self.get('/users/%(user)s/projects' % {'user': user['id']})
         self.assertValidProjectListResponse(resp)
         self.assertEqual(project['id'], resp.result['projects'][0]['id'])
+
+
+class StrictTwoLevelLimitsResourceTestCase(ResourceTestCase):
+    def setUp(self):
+        super(StrictTwoLevelLimitsResourceTestCase, self).setUp()
+
+    def config_overrides(self):
+        super(StrictTwoLevelLimitsResourceTestCase, self).config_overrides()
+        self.config_fixture.config(group='unified_limit',
+                                   enforcement_model='strict_two_level')
+
+    def _create_projects_hierarchy(self, hierarchy_size=1):
+        if hierarchy_size > 1:
+            self.skip_test_overrides(
+                "Strict two level limit enforcement model doesn't allow the"
+                "project tree depth > 2")
+        return super(StrictTwoLevelLimitsResourceTestCase,
+                     self)._create_projects_hierarchy(hierarchy_size)
+
+    def test_create_hierarchical_project(self):
+        projects = self._create_projects_hierarchy()
+
+        # create grandchild project will fail.
+        new_ref = unit.new_project_ref(
+            domain_id=self.domain_id,
+            parent_id=projects[1]['project']['id'])
+        self.post('/projects',
+                  body={'project': new_ref},
+                  expected_status=http_client.FORBIDDEN)
