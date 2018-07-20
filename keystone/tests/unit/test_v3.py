6dbd0058421feb9311571b4a921831a7b35bdc72
Morgan Fainberg <morgan.fainberg@gmail.com>
2018-07-20 14:51:01 -0700

    Remove direct calls to auth.controllers in some tests
    Tests in keystone.tests.unit.test_v3 and kestone.tests.unit.test_v3_auth
    no longer directly instantiate or calls to the auth controllers. The
    only exception are test classes that are not used, those have had a
    TODO note added to them. This is being done for the conversion to flask
    where the auth controllers will no longer exist. All test changes are
    done outside of the conversion of webob controller -> flask restful
    api.
    
    Change-Id: Ib668ca18faf42b41bc63558c6634fca96224d195
    Partial-Bug: #1776504

--- keystone/tests/unit/test_v3.py
+++ keystone/tests/unit/test_v3.py
@@ -1229,15 +1229,20 @@ class RestfulTestCase(unit.SQLDriverOverrides, rest.RestfulTestCase,
     def build_external_auth_request(self, remote_user,
                                     remote_domain=None, auth_data=None,
                                     kerberos=False):
-        environment = {'REMOTE_USER': remote_user, 'AUTH_TYPE': 'Negotiate'}
-        if remote_domain:
-            environment['REMOTE_DOMAIN'] = remote_domain
+        environment = self.build_external_auth_environ(
+            remote_user, remote_domain)
         if not auth_data:
             auth_data = self.build_authentication_request(
                 kerberos=kerberos)['auth']
         auth_info = auth.core.AuthInfo.create(auth_data)
         auth_context = auth.core.AuthContext(extras={}, method_names=[])
         return self.make_request(environ=environment), auth_info, auth_context
+
+    def build_external_auth_environ(self, remote_user, remote_domain=None):
+        environment = {'REMOTE_USER': remote_user, 'AUTH_TYPE': 'Negotiate'}
+        if remote_domain:
+            environment['REMOTE_DOMAIN'] = remote_domain
+        return environment
 
 
 class VersionTestCase(RestfulTestCase):
