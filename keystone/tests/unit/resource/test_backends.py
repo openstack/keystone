44da48f013881e87f8ac099cf7462d00d804ddd3
wangxiyuan <wangxiyuan@huawei.com>
2018-08-06 15:05:25 +0800

    Remove redundant get_project call
    This patched removed some redundant "get_project" calls when
    deleting projects/domains.
    
    Change-Id: Ife4dd18962077bac30fa1cecf7621cc86a62929c
    Closes-bug: #1764282

--- keystone/tests/unit/resource/test_backends.py
+++ keystone/tests/unit/resource/test_backends.py
@@ -21,6 +21,7 @@ from keystone.common import driver_hints
 from keystone.common import provider_api
 import keystone.conf
 from keystone import exception
+from keystone.resource.backends import sql as resource_sql
 from keystone.tests import unit
 from keystone.tests.unit import default_fixtures
 from keystone.tests.unit import utils as test_utils
@@ -1293,6 +1294,23 @@ class ResourceTests(object):
         self.assertRaises(exception.DomainNotFound,
                           PROVIDERS.resource_api.get_domain,
                           domain['id'])
+
+    @unit.skip_if_no_multiple_domains_support
+    def test_delete_domain_call_db_time(self):
+        domain = unit.new_domain_ref()
+        PROVIDERS.resource_api.create_domain(domain['id'], domain)
+        # Disable the domain
+        domain['enabled'] = False
+        PROVIDERS.resource_api.update_domain(domain['id'], domain)
+
+        domain_ref = PROVIDERS.resource_api.get_project(domain['id'])
+        with mock.patch.object(resource_sql.Resource,
+                               "get_project") as mock_get_project:
+
+            mock_get_project.return_value = domain_ref
+            # Delete the domain
+            PROVIDERS.resource_api.delete_domain(domain['id'])
+            self.assertEqual(mock_get_project.call_count, 1)
 
     @unit.skip_if_no_multiple_domains_support
     def test_domain_name_case_sensitivity(self):
