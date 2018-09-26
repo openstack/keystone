0dc5c4edabd5cb0455ffe1c4f8cf8369f64b2197
Lance Bragstad <lbragstad@gmail.com>
2018-09-26 21:34:13 +0000

    Pass context objects to policy enforcement
    The oslo.policy library actually accepts context objects as a first
    class citizen, instead of a hand-built `creds` dictionary. This is a
    perferred approach because it's easier for services to use
    oslo.context to generate a context object that they can automatically
    pass to oslo.policy for enforcement instead of inspecting the context
    object and building a dictionary manually to pass to oslo.policy.
    
    This commit makes allows keystone to partake in this by pulling the
    keystone request object, which is a subclass of oslo.context's
    RequestContext object, and uses it in enforcement. Additionally,
    we're overriding the to_policy_values() method of oslo.context
    in order to make sure we port keystone-specific values to the policy
    dict representation of a context object. This ensures we have values
    present that we rely on with our default policies.
    
    This commit also bumps the lower requirement for oslo.policy to
    make sure we're always using a version that understands context
    objects.
    
    Change-Id: I63e713f4aebf3e8cf5189a6060569d2828bc364d

--- keystone/common/context.py
+++ keystone/common/context.py
@@ -38,3 +38,30 @@ class RequestContext(oslo_context.RequestContext):
 
         self.authenticated = kwargs.pop('authenticated', False)
         super(RequestContext, self).__init__(**kwargs)
+
+    def to_policy_values(self):
+        """Add keystone-specific policy values to policy representation.
+
+        This method converts generic policy values to a dictionary form using
+        the base implementation from oslo_context.context.RequestContext.
+        Afterwards, it is going to pull keystone-specific values off the
+        context and represent them as items in the policy values dictionary.
+        This is because keystone uses default policies that rely on these
+        values, so we need to guarantee they are present during policy
+        enforcement if they are present on the context object.
+
+        This method is automatically called in
+        oslo_policy.policy.Enforcer.enforce() if oslo.policy knows it's dealing
+        with a context object.
+
+        """
+        # TODO(morgan): Rework this to not need an explicit token render as
+        # this is a generally poorly designed behavior. The enforcer should not
+        # rely on a contract of the token's rendered JSON form. This likely
+        # needs reworking of how we handle the context in oslo.policy. Until
+        # this is reworked, it is not possible to merge the token render
+        # function into keystone.api
+        values = super(RequestContext, self).to_policy_values()
+        values['token'] = self.token_reference['token']
+        values['domain_id'] = self.domain_id if self.domain_id else None
+        return values
