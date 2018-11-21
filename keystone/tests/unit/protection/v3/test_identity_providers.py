a4c5d804395f20d0c8832ae6ed9a7594926bf981
Lance Bragstad <lbragstad@gmail.com>
2018-11-21 21:58:24 +0000

    Update idp policies for system admin
    This change makes the policy definitions for admin idp operations
    consistent with the other idp policies. Subsequent patches will
    incorporate:
    
     - domain users test coverage
     - project users test coverage
    
     Related-Bug: 1804517
     Closes-Bug: 1804516
    
    Change-Id: I6d6a19d95d8970362993c83e70cf23c989ae45e3

--- keystone/tests/unit/protection/v3/test_identity_providers.py
+++ keystone/tests/unit/protection/v3/test_identity_providers.py
@@ -161,3 +161,64 @@ class SystemMemberTests(base_classes.TestCaseWithBootstrap,
             r = c.post('/v3/auth/tokens', json=auth)
             self.token_id = r.headers['X-Subject-Token']
             self.headers = {'X-Auth-Token': self.token_id}
+
+
+class SystemAdminTests(base_classes.TestCaseWithBootstrap,
+                       common_auth.AuthTestMixin,
+                       _SystemUserIdentityProviderTests):
+
+    def setUp(self):
+        super(SystemAdminTests, self).setUp()
+        self.loadapp()
+        self.useFixture(ksfixtures.Policy(self.config_fixture))
+        self.config_fixture.config(group='oslo_policy', enforce_scope=True)
+
+        # Reuse the system administrator account created during
+        # ``keystone-manage bootstrap``
+        self.user_id = self.bootstrapper.admin_user_id
+        auth = self.build_authentication_request(
+            user_id=self.user_id,
+            password=self.bootstrapper.admin_password,
+            system=True
+        )
+
+        # Grab a token using the persona we're testing and prepare headers
+        # for requests we'll be making in the tests.
+        with self.test_client() as c:
+            r = c.post('/v3/auth/tokens', json=auth)
+            self.token_id = r.headers['X-Subject-Token']
+            self.headers = {'X-Auth-Token': self.token_id}
+
+    def test_user_can_create_identity_providers(self):
+        create = {'identity_provider': {'remote_ids': [uuid.uuid4().hex]}}
+
+        with self.test_client() as c:
+            c.put(
+                '/v3/OS-FEDERATION/identity_providers/%s' % uuid.uuid4().hex,
+                json=create, headers=self.headers,
+                expected_status_code=http_client.CREATED
+            )
+
+    def test_user_can_update_identity_providers(self):
+        idp = PROVIDERS.federation_api.create_idp(
+            uuid.uuid4().hex, unit.new_identity_provider_ref()
+        )
+
+        update = {'identity_provider': {'enabled': False}}
+
+        with self.test_client() as c:
+            c.patch(
+                '/v3/OS-FEDERATION/identity_providers/%s' % idp['id'],
+                json=update, headers=self.headers
+            )
+
+    def test_user_can_delete_identity_providers(self):
+        idp = PROVIDERS.federation_api.create_idp(
+            uuid.uuid4().hex, unit.new_identity_provider_ref()
+        )
+
+        with self.test_client() as c:
+            c.delete(
+                '/v3/OS-FEDERATION/identity_providers/%s' % idp['id'],
+                headers=self.headers
+            )
