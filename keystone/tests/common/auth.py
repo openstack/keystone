e5d1eba024aae12e05e30c42bd3e3d9c97d398a4
Lance Bragstad <lbragstad@gmail.com>
2018-11-28 14:23:41 +0000

    Add missing translation import to common.auth.py
    This utility was refactored, but the refactor didn't include a
    required import statement:
    
      Ie7e156a83006c1578c87d862cb4d1d948a800809
    
    This wasn't caught because non of our unit tests were trying to
    build authentication payloads with a bogus scope.
    
    Change-Id: I43f72d12f3eb57af69c4b40258baa3b11d0a6a54

--- keystone/tests/common/auth.py
+++ keystone/tests/common/auth.py
@@ -10,6 +10,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from keystone.i18n import _
+
 
 class AuthTestMixin(object):
     """To hold auth building helper functions."""
