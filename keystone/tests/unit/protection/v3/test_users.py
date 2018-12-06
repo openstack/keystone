29fb7ae3952138ec05fd2434fe9cf5eccc8205f0
Lance Bragstad <lbragstad@gmail.com>
2018-12-06 19:59:36 +0000

    Implement system admin role in users API
    This commit introduces the system admin role to the users API,
    making it consistent with other system-admin policy definitions.
    
    Subsequent patches will build on this work to expose more
    functionality to domain and project users:
    
     - domain reader functionality
     - domain member test coverage
     - domain admin functionality
     - project user test coverage
    
    Change-Id: I19bf5a562401100d9208f98515ce596f7ca20185
    Closes-Bug: 1805406
    Partial-Bug: 1748027
    Partial-Bug: 968696

--- keystone/tests/unit/protection/v3/test_users.py
+++ keystone/tests/unit/protection/v3/test_users.py
@@ -200,3 +200,75 @@ class SystemMemberTests(base_classes.TestCaseWithBootstrap,
             r = c.post('/v3/auth/tokens', json=auth)
             self.token_id = r.headers['X-Subject-Token']
             self.headers = {'X-Auth-Token': self.token_id}
+
+
+class SystemAdminTests(base_classes.TestCaseWithBootstrap,
+                       common_auth.AuthTestMixin,
+                       _CommonUserTests,
+                       _SystemUserTests):
+
+    def setUp(self):
+        super(SystemAdminTests, self).setUp()
+        self.loadapp()
+        self.useFixture(ksfixtures.Policy(self.config_fixture))
+        self.config_fixture.config(group='oslo_policy', enforce_scope=True)
+
+        self.user_id = self.bootstrapper.admin_user_id
+        auth = self.build_authentication_request(
+            user_id=self.user_id,
+            password=self.bootstrapper.admin_password,
+            system=True
+        )
+
+        # Grab a token using the persona we're testing and prepare headers
+        # for requests we'll be making in the tests.
+        with self.test_client() as c:
+            r = c.post('/v3/auth/tokens', json=auth)
+            self.token_id = r.headers['X-Subject-Token']
+            self.headers = {'X-Auth-Token': self.token_id}
+
+    def test_user_can_create_users(self):
+        create = {
+            'user': {
+                'name': uuid.uuid4().hex,
+                'domain': CONF.identity.default_domain_id
+            }
+        }
+
+        with self.test_client() as c:
+            c.post('/v3/users', json=create, headers=self.headers)
+
+    def test_user_can_update_users(self):
+        user = PROVIDERS.identity_api.create_user(
+            unit.new_user_ref(domain_id=CONF.identity.default_domain_id)
+        )
+
+        update = {'user': {'email': uuid.uuid4().hex}}
+        with self.test_client() as c:
+            c.patch(
+                '/v3/users/%s' % user['id'], json=update, headers=self.headers
+            )
+
+    def test_user_cannot_update_non_existent_user_not_found(self):
+        update = {'user': {'email': uuid.uuid4().hex}}
+        with self.test_client() as c:
+            c.patch(
+                '/v3/users/%s' % uuid.uuid4().hex, json=update,
+                headers=self.headers,
+                expected_status_code=http_client.NOT_FOUND
+            )
+
+    def test_user_can_delete_users(self):
+        user = PROVIDERS.identity_api.create_user(
+            unit.new_user_ref(domain_id=CONF.identity.default_domain_id)
+        )
+
+        with self.test_client() as c:
+            c.delete('/v3/users/%s' % user['id'], headers=self.headers)
+
+    def test_user_cannot_delete_non_existent_user_not_found(self):
+        with self.test_client() as c:
+            c.delete(
+                '/v3/users/%s' % uuid.uuid4().hex, headers=self.headers,
+                expected_status_code=http_client.NOT_FOUND
+            )
