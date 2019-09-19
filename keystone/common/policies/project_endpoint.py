c9148db371dfa449830d2fbe7c1345135ebddf3f
Vishakha Agarwal <agarwalvishakha18@gmail.com>
2019-09-19 18:45:30 +0530

    Implement scope type checking for Project Endpoints
    This change updates the Project Endpoints policies to understand
    the scope types for Project Endpoints. This adds the test cases
    too.
    
    Change-Id: Id18036325b2f5b8836076408ecdd64523b19cbba
    Closes-Bug: #1844664

--- keystone/common/policies/project_endpoint.py
+++ keystone/common/policies/project_endpoint.py
@@ -10,15 +10,50 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from oslo_log import versionutils
 from oslo_policy import policy
 
 from keystone.common.policies import base
+
+deprecated_list_projects_for_endpoint = policy.DeprecatedRule(
+    name=base.IDENTITY % 'list_projects_for_endpoint',
+    check_str=base.RULE_ADMIN_REQUIRED,
+)
+
+deprecated_add_endpoint_to_project = policy.DeprecatedRule(
+    name=base.IDENTITY % 'add_endpoint_to_project',
+    check_str=base.RULE_ADMIN_REQUIRED,
+)
+
+deprecated_check_endpoint_in_project = policy.DeprecatedRule(
+    name=base.IDENTITY % 'check_endpoint_in_project',
+    check_str=base.RULE_ADMIN_REQUIRED,
+)
+
+deprecated_list_endpoints_for_project = policy.DeprecatedRule(
+    name=base.IDENTITY % 'list_endpoints_for_project',
+    check_str=base.RULE_ADMIN_REQUIRED,
+)
+
+deprecated_remove_endpoint_from_project = policy.DeprecatedRule(
+    name=base.IDENTITY % 'remove_endpoint_from_project',
+    check_str=base.RULE_ADMIN_REQUIRED,
+)
+
+DEPRECATED_REASON = """
+As of the Train release, the project endpoint API now understands default
+roles and system-scoped tokens, making the API more granular by default without
+compromising security. The new policy defaults account for these changes
+automatically. Be sure to take these new defaults into consideration if you are
+relying on overrides in your deployment for the project endpoint API.
+"""
+
 
 project_endpoint_policies = [
 
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'list_projects_for_endpoint',
-        check_str=base.RULE_ADMIN_REQUIRED,
+        check_str=base.SYSTEM_READER,
         # NOTE(lbragstad): While projects can be considered project-level APIs
         # with hierarchical multi-tenancy, endpoints are a system-level
         # resource. Managing associations between projects and endpoints should
@@ -27,18 +62,24 @@ project_endpoint_policies = [
         description='List projects allowed to access an endpoint.',
         operations=[{'path': ('/v3/OS-EP-FILTER/endpoints/{endpoint_id}/'
                               'projects'),
-                     'method': 'GET'}]),
+                     'method': 'GET'}],
+        deprecated_rule=deprecated_list_projects_for_endpoint,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.TRAIN),
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'add_endpoint_to_project',
-        check_str=base.RULE_ADMIN_REQUIRED,
+        check_str=base.SYSTEM_ADMIN,
         scope_types=['system'],
         description='Allow project to access an endpoint.',
         operations=[{'path': ('/v3/OS-EP-FILTER/projects/{project_id}/'
                               'endpoints/{endpoint_id}'),
-                     'method': 'PUT'}]),
+                     'method': 'PUT'}],
+        deprecated_rule=deprecated_add_endpoint_to_project,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.TRAIN),
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'check_endpoint_in_project',
-        check_str=base.RULE_ADMIN_REQUIRED,
+        check_str=base.SYSTEM_READER,
         scope_types=['system'],
         description='Check if a project is allowed to access an endpoint.',
         operations=[{'path': ('/v3/OS-EP-FILTER/projects/{project_id}/'
@@ -46,24 +87,33 @@ project_endpoint_policies = [
                      'method': 'GET'},
                     {'path': ('/v3/OS-EP-FILTER/projects/{project_id}/'
                               'endpoints/{endpoint_id}'),
-                     'method': 'HEAD'}]),
+                     'method': 'HEAD'}],
+        deprecated_rule=deprecated_check_endpoint_in_project,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.TRAIN),
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'list_endpoints_for_project',
-        check_str=base.RULE_ADMIN_REQUIRED,
+        check_str=base.SYSTEM_READER,
         scope_types=['system'],
         description='List the endpoints a project is allowed to access.',
         operations=[{'path': ('/v3/OS-EP-FILTER/projects/{project_id}/'
                               'endpoints'),
-                     'method': 'GET'}]),
+                     'method': 'GET'}],
+        deprecated_rule=deprecated_list_endpoints_for_project,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.TRAIN),
     policy.DocumentedRuleDefault(
         name=base.IDENTITY % 'remove_endpoint_from_project',
-        check_str=base.RULE_ADMIN_REQUIRED,
+        check_str=base.SYSTEM_ADMIN,
         scope_types=['system'],
         description=('Remove access to an endpoint from a project that has '
                      'previously been given explicit access.'),
         operations=[{'path': ('/v3/OS-EP-FILTER/projects/{project_id}/'
                               'endpoints/{endpoint_id}'),
-                     'method': 'DELETE'}])
+                     'method': 'DELETE'}],
+        deprecated_rule=deprecated_remove_endpoint_from_project,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.TRAIN),
 ]
 
 
