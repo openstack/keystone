39746b74179c996954b1e5f4ed94517e0b4af2d6
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-05-02 16:33:20 +0900

    Remove templated catalog driver
    The driver was deprecated in 2024.1 release because of the unmaintained
    default template[1].
    
    [1] 307296af5e170ca6b0d44fd5ec85a39bd6b5e572
    
    Change-Id: If7b040a39679b2707565ecd0bd8a06b7db7503c1

--- keystone/tests/unit/test_v3_catalog.py
+++ keystone/tests/unit/test_v3_catalog.py
@@ -1030,34 +1030,3 @@ class TestCatalogAPISQLRegions(unit.TestCase):
         for k in keys:
             self.assertEqual(ref.get(k), entity[k], k)
         self.assertEqual(entity['region_id'], entity['region'])
-
-
-class TestCatalogAPITemplatedProject(test_v3.RestfulTestCase):
-    """Templated Catalog doesn't support full API.
-
-    Eg. No region/endpoint creation.
-
-    """
-
-    def config_overrides(self):
-        super().config_overrides()
-        self.config_fixture.config(group='catalog', driver='templated')
-
-    def load_fixtures(self, fixtures):
-        self.load_sample_data(create_region_and_endpoints=False)
-
-    def test_project_delete(self):
-        """Deleting a project should not result in an 500 ISE.
-
-        Deleting a project will create a notification, which the EndpointFilter
-        functionality will use to clean up any project->endpoint and
-        project->endpoint_group relationships. The templated catalog does not
-        support such relationships, but the act of attempting to delete them
-        should not cause a NotImplemented exception to be exposed to an API
-        caller.
-
-        Deleting an endpoint has a similar notification and clean up
-        mechanism, but since we do not allow deletion of endpoints with the
-        templated catalog, there is no testing to do for that action.
-        """
-        self.delete(f'/projects/{self.project_id}')
