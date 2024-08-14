b00cb4197d603da948f8f43f9c5f0e55182d2c96
Artem Goncharov <artem.goncharov@gmail.com>
2024-08-14 20:12:10 +0200

    Prevent circlular imports in keystone.api
    keystone.api.__init__ is importing all submodules without real need. It
    is only used to make it easier to load all api blueprints.
    
    This, however, leads to potential circular imports when inside
    `keystone.api` we import something outside `keystone.api` what is again
    importing `keystone.api`. One example here started popping in the new
    validation framework which is located under `keystone.api` as well. We
    try decorate api method with schema which imports parameter_types from
    the `keystone.api.validation`. Since python executes all the `__init__`
    of the package a simple `from keystone.application_credential import
    schema` cause circular import (`schema` imports `api.validation` which
    implicitly imports all submodules importing all other schemas including
    the one we requested initially).
    
    On the other side we also do not want that `from keystone.api import
    auth` would import all other submodules without anybody needing that.
    
    Change-Id: I613ae73f469321d2bbfaf74c4b3365c3365be9a8

--- keystone/server/flask/application.py
+++ keystone/server/flask/application.py
@@ -25,7 +25,33 @@ except ImportError:
     # werkzeug 0.14.x
     import werkzeug.wsgi as wsgi_dispatcher
 
-import keystone.api
+from keystone.api import auth
+from keystone.api import credentials
+from keystone.api import discovery
+from keystone.api import domains
+from keystone.api import ec2tokens
+from keystone.api import endpoints
+from keystone.api import groups
+from keystone.api import limits
+from keystone.api import os_ep_filter
+from keystone.api import os_federation
+from keystone.api import os_inherit
+from keystone.api import os_oauth1
+from keystone.api import os_oauth2
+from keystone.api import os_revoke
+from keystone.api import os_simple_cert
+from keystone.api import policy
+from keystone.api import projects
+from keystone.api import regions
+from keystone.api import registered_limits
+from keystone.api import role_assignments
+from keystone.api import role_inferences
+from keystone.api import roles
+from keystone.api import s3tokens
+from keystone.api import services
+from keystone.api import system
+from keystone.api import trusts
+from keystone.api import users
 from keystone import exception
 from keystone.oauth2 import handlers as oauth2_handlers
 from keystone.receipt import handlers as receipt_handlers
@@ -34,6 +60,36 @@ from keystone.server.flask.request_processing import json_body
 from keystone.server.flask.request_processing import req_logging
 
 LOG = log.getLogger(__name__)
+
+__apis__ = (
+    discovery,
+    auth,
+    credentials,
+    domains,
+    ec2tokens,
+    endpoints,
+    groups,
+    limits,
+    os_ep_filter,
+    os_federation,
+    os_inherit,
+    os_oauth1,
+    os_oauth2,
+    os_revoke,
+    os_simple_cert,
+    policy,
+    projects,
+    regions,
+    registered_limits,
+    role_assignments,
+    role_inferences,
+    roles,
+    s3tokens,
+    services,
+    system,
+    trusts,
+    users,
+)
 
 
 def fail_gracefully(f):
@@ -170,7 +226,7 @@ def application_factory(name='public'):
         PROPAGATE_EXCEPTIONS=True
     )
 
-    for api in keystone.api.__apis__:
+    for api in __apis__:
         for api_bp in api.APIs:
             api_bp.instantiate_and_register_to_app(app)
 
--- keystone/tests/unit/core.py
+++ keystone/tests/unit/core.py
@@ -44,7 +44,6 @@ from oslo_log import log
 from oslo_utils import timeutils
 import testtools
 
-import keystone.api
 from keystone.common import context
 from keystone.common import json_home
 from keystone.common import provider_api
